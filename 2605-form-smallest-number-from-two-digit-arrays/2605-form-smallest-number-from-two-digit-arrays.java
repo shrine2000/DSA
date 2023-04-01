e10ca76094a050415ed1b626d7d2c9ca24b3ce76
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-01 22:44:45 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (100.00%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
@@ -3,19 +3,18 @@ class Solution {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
+        int bitmask = 0;
 
-        int[] count = new int[10];
         for (int i : nums1) {
-            count[i]++;
-        }
-        for (int j : nums2) {
-            count[j]++;
+            bitmask |= (1 << i);
         }
 
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] >= 2) return i;
+        for (int j : nums2) {
+            if ((bitmask & (1 << j)) != 0) {
+                return j;
+            }
         }
-        
-        return Math.min((nums1[0]*10+nums2[0]),(nums2[0]*10+nums1[0]));
+
+        return Math.min((nums1[0] * 10 + nums2[0]), (nums2[0] * 10 + nums1[0]));
     }
 }
