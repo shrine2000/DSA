3b40a10068c46791ba5fbd4edc5f94465fd200ae
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-01 22:58:03 +0530

    Time: 1 ms (99.87%), Space: 40.4 MB (36.28%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -1,5 +1,21 @@
 class Solution {
-    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        return Math.min(k, numOnes) - Math.max(0, k - numOnes - numZeros);
-    }
+    public int kItemsWithMaximumSum(int ones, int zeros, int negOnes, int k) {
+            int sum = 0;
+            // Pick items one by one until k items are picked
+            while(k-- > 0){
+                if(ones > 0){
+                    // Pick an item with "1" written on it if available
+                    sum++;
+                    ones--;
+                }else if(zeros > 0){
+                    // Pick an item with "0" written on it if available
+                    zeros--;
+                }else{
+                    // Pick an item with "-1" written on it if available
+                    sum--;
+                    negOnes--;
+                }
+            }
+            return sum;
+        }
 }
