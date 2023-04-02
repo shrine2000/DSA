ad44efd61385c81fc5566e8765271f2b6b3f54ae
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-02 13:02:46 +0530

    Time: 1 ms (98.98%), Space: 43.2 MB (10.14%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,22 +1,27 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        BitSet bst1 = new BitSet();
-        for (int num : nums1) {
-            bst1.set(num);
+        List<Integer> commonElements = new ArrayList<>(); // to store the common elements
+        boolean[] elementExists = new boolean[1001]; // to keep track of which elements exist in nums1
+
+        // Iterate over the elements in nums1 and mark them as existing in the elementExists array
+        for (int i = 0; i < nums1.length; i++) {
+            elementExists[nums1[i]] = true;
         }
 
-        BitSet bst2 = new BitSet();
-        for (int num : nums2) {
-            bst2.set(num);
+        // Iterate over the elements in nums2 and check if each exists in the elementExists array
+        for (int i = 0; i < nums2.length; i++) {
+            int currentElement = nums2[i];
+            if (elementExists[currentElement]) { // currentElement exists in nums1
+                commonElements.add(currentElement); // add currentElement to the list of common elements
+                elementExists[currentElement] = false; // mark currentElement as no longer existing in nums1
+            }
         }
 
-        bst1.and(bst2);
-        int[] result = new int[bst1.cardinality()]; //  returns the number of bits that are set in bst1
-        int i = 0;
-        for (int num = bst1.nextSetBit(0); num >= 0; num = bst1.nextSetBit(num + 1)) {
-            result[i++] = num;
+        // Convert the ArrayList of common elements to an array of integers and return it
+        int[] result = new int[commonElements.size()];
+        for (int i = 0; i < commonElements.size(); i++) {
+            result[i] = commonElements.get(i);
         }
-
         return result;
     }
 }
