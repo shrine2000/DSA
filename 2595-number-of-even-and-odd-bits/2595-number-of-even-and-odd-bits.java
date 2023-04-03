8c3fab690421459e25b1c2b071eddd82ab67141d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-03 12:53:49 +0530

    Time: 1 ms (99.96%), Space: 42.5 MB (71.84%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] evenOddBit(int n) {
-        int even = 0, odd = 0;
+       int i = 0, even = 0, odd = 0;
 
-        for (int i = 0; i < 32; i++) {
-            if (((n >> i) & 1) == 1) {
-                if ((i & 1) == 0) {
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                if (i % 2 == 0) {
                     even++;
                 } else {
                     odd++;
                 }
             }
+            n >>= 1; // shift bits to the right by 1
+            i++;
         }
- 
+
         return new int[]{even, odd};
     }
 }
