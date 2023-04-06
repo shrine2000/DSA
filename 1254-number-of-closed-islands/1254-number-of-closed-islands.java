cf4be5673f942030230e7617f4775b50d19d910a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-06 10:42:27 +0530

    Time: 2 ms (82.36%), Space: 43.6 MB (11.94%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,52 +1,45 @@
 class Solution {
-    public int closedIsland(int[][] grid) {
-        
-        // get the number of rows and columns in the grid
-        int m = grid.length;
-        int n = grid[0].length;
+     public int closedIsland(int[][] gd) {
 
-        // traverse the left and right edges of the grid and call dfs on each cell that is 0
-        for (int i=0; i<m; i++) {
-            dfs(grid, i, 0);
-            dfs(grid, i, n-1);
-        }
+            int m = gd.length;
+            int n = gd[0].length;
 
-        // traverse the top and bottom edges of the grid and call dfs on each cell that is 0
-        for (int j=0; j<n; j++) {
-            dfs(grid, 0, j);
-            dfs(grid, m-1, j);
-        }
+            for (int i=0; i<m; i++) {
+                dfs(gd, i, 0);
+                dfs(gd, i, n-1);
+            }
 
-        // count the number of closed islands
-        int island = 0;
-        for (int k=0; k<m; k++) {
-            for (int l=0; l<n; l++) {
-                if (grid[k][l] == 0) {
-                    island++;
-                    dfs(grid, k, l);
+            for (int j=0; j<n; j++) {
+                dfs(gd, 0, j);
+                dfs(gd, m-1, j);
+            }
+
+            int isd = 0;
+            for (int k=0; k<m; k++) {
+                for (int l=0; l<n; l++) {
+                    if (gd[k][l] == 0) {
+                        isd++;
+                        dfs(gd, k, l);
+                    }
                 }
             }
+
+            return isd;
         }
 
-        return island;
-    }
 
+        private void dfs (int[][] gd, int r, int c) {
 
-    private void dfs (int[][] grid, int row, int col) {
+            if (r < 0 || r > gd.length-1 || c <0 || c>gd[0].length-1 || gd[r][c] != 0) {
+                return;
+            }
 
-        // if the current cell is out of bounds or has already been visited or is not 0, return
-        if (row < 0 || row > grid.length-1 || col <0 || col>grid[0].length-1 || grid[row][col] != 0) {
-            return;
-        } 
+            gd[r][c] = -1;
 
-        // mark the current cell as visited
-        grid[row][col] = -1;
+            dfs(gd, r+1, c);
+            dfs(gd, r-1, c);
+            dfs(gd, r, c+1);
+            dfs(gd, r, c-1);
 
-        // call dfs on each of the neighboring cells that are 0
-        dfs(grid, row+1, col);
-        dfs(grid, row-1, col);
-        dfs(grid, row, col+1);
-        dfs(grid, row, col-1);
-
-    }
+        }
 }
