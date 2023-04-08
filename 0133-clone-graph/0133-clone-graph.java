6b80f233d25183fb95ff0a80aba51ee2cd351ff4
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-08 22:03:40 +0530

    Time: 26 ms (45.98%), Space: 42.5 MB (62.89%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,25 +19,29 @@ class Node {
 */
 
 class Solution {
-    public Node cloneGraph(Node node) {
-        if(node==null) return null;
-        Queue<Node> q = new ArrayDeque<>(Arrays.asList(node));
-        Map<Node, Node> map = new HashMap<>();
-        map.put(node, new Node(node.val));
+    public Node cloneGraph(Node originalNode) {
+        if(originalNode == null) {
+            return null;
+        }
         
-        while(!q.isEmpty()){
-            Node u = q.poll();
-            for(Node v : u.neighbors) {
-                if(!map.containsKey(v)) {
-                    map.put(v, new Node(v.val));
-                    q.offer(v);
-                }
-                map.get(u).neighbors.add(map.get(v));
-
-            }
+        Queue<Node> bfsQueue = new ArrayDeque<>(Arrays.asList(originalNode));
+        Map<Node, Node> originalToCloneMap = new HashMap<>();
+        originalToCloneMap.put(originalNode, new Node(originalNode.val));
+        
+        while(!bfsQueue.isEmpty()){
+            Node currentNode = bfsQueue.poll();
+            Node clonedNode = originalToCloneMap.get(currentNode);
             
-         }
+            for(Node originalNeighbor : currentNode.neighbors) {
+                if(!originalToCloneMap.containsKey(originalNeighbor)) {
+                    Node clonedNeighbor = new Node(originalNeighbor.val);
+                    originalToCloneMap.put(originalNeighbor, clonedNeighbor);
+                    bfsQueue.offer(originalNeighbor);
+                }
+                clonedNode.neighbors.add(originalToCloneMap.get(originalNeighbor));
+            }
+        }
         
-        return map.get(node);
+        return originalToCloneMap.get(originalNode);
     }
 }
