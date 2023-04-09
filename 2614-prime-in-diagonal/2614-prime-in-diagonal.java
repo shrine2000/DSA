5471b377d18a5f55f434b30e95bec067bb261461
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-09 10:19:14 +0530

    Time: 7 ms (20.00%), Space: 51.4 MB (100.00%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -1,22 +1,27 @@
 class Solution {
-      private boolean isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) return false;
-        }
+     public boolean isPrime(int n){
+        if (n <= 1) return false;
+        for(int i = 2;i * i <= n;i++) if(n % i == 0) return false;
         return true;
     }
     
     public int diagonalPrime(int[][] nums) {
         int n = nums.length;
-        int largestPrime = 0;
-        for (int i = 0; i < n; i++) {
-            if (isPrime(nums[i][i])) largestPrime = Math.max(largestPrime, nums[i][i]);
-            if (isPrime(nums[i][n-i-1])) largestPrime = Math.max(largestPrime, nums[i][n-i-1]);
+        int maxPrime = 0;
+        
+        // check main diagonal
+        for(int i = 0;i < n;i++){
+            int num = nums[i][i];
+            if(isPrime(num)) maxPrime = Math.max(maxPrime, num);
         }
-        return largestPrime;
+        
+        // check secondary diagonal
+        for(int i = 0;i < n;i++){
+            int num = nums[i][n - i - 1];
+            if(isPrime(num)) maxPrime = Math.max(maxPrime, num);
+        }
+        
+        return maxPrime;
     }
 
  
