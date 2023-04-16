846821ae5394dc4078a1021ea1fa96eb30c94b3b
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-16 22:08:35 +0530

    Time: 2 ms (80.00%), Space: 44.8 MB (20.00%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int[] rowAndMaximumOnes(int[][] mat) {
-      int max_row = 0;
-        int max_count = 0;
+    public int[] rowAndMaximumOnes(int[][] matrix) {
+      int[] answer = new int[]{0, 0};
         
-        for (int i = 0; i < mat.length; i++) {
-            int oneCount = 0;
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 1) {
-                    oneCount++;
-                }
+        for (int i = 0; i < matrix.length; i++) {
+            int count = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                count += matrix[i][j];
             }
-            if (oneCount > max_count) {
-                max_count = oneCount;
-                max_row = i;
+            
+            if (count > answer[1]) {
+                answer[0] = i;
+                answer[1] = count;
             }
         }
-        
-        return new int[] {max_row, max_count};
+        return answer;
     }
 }
