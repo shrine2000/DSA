04320d3784f18b2176b6776722a571c45dc9d3e3
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-19 14:40:23 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (97.36%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -1,19 +1,25 @@
 class Solution {
     public boolean areNumbersAscending(String s) {
-        String[] tokens = s.split(" ");
-        int previousNumber = 0;
-
-        for (String token : tokens) {
-            if (Character.isDigit(token.charAt(0))) {
-                int currentNumber = Integer.parseInt(token);
+        int previousNumber = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            int currentChar = s.charAt(i);
+            int currentNumber = 0;
+            int j = i;
+            if (currentChar >= 48 && currentChar <= 57) {
+                while (currentChar >= 48 && currentChar <= 57 && i < s.length()) {
+                    currentNumber = currentNumber * 10 + (currentChar - 48);
+                    i++;
+                    if (i == s.length()) {
+                        break;
+                    }
+                    currentChar = s.charAt(i);
+                }
                 if (currentNumber <= previousNumber) {
                     return false;
-                } else {
-                    previousNumber = currentNumber;
                 }
+                previousNumber = currentNumber;
             }
         }
-
         return true;
     }
 }
