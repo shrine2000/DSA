ac4f93afbced0d647a9ab4098d96ee7efcd50bea
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-28 00:09:50 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (24.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        int topIndex = -1;
+        char[] stack = new char[s.length()];
+        
+        for(char currentChar : s.toCharArray()){
+            if(currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack[++topIndex] = currentChar;
+            } else {
+                if(topIndex >= 0 && ((stack[topIndex] == '(' && currentChar == ')') || (stack[topIndex] == '[' && currentChar == ']') || (stack[topIndex] == '{' && currentChar == '}'))) {
+                    topIndex--;
+                } else return false;
+            }
+        }
+        
+        return topIndex==-1;                            
+    }
+}
