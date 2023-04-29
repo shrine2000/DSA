4e22ce85a8c246ad17d019b1e48f17b062cd1a46
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-29 20:14:10 +0530

    Time: 1 ms (100.00%), Space: 51.7 MB (44.12%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,29 +1,35 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
+        int sum = 0, max = 0;
         
-        int totalSum = 0;
-        for(int i =0; i < n; i++){
-            totalSum += cardPoints[i];
+        // Handle edge case where k >= n
+        if(k >= n){
+            for(int i : cardPoints) sum += i;
+            return sum;
         }
         
-        if(k == n) {
-            return totalSum;
+        // Calculate initial sum of first k elements
+        for(int i = 0; i < k; i++){
+            sum += cardPoints[i];
         }
         
-        int windowSum = 0;
-        for(int i = 0; i < n -k; i++){
-            windowSum += cardPoints[i];
+        // Move sliding window to the right and update max sum
+        int ptr = k - 1;
+        max = sum;
+        int j = n - 1;
+        int loop = 0;
+        while(loop < k && ptr >=0){
+            sum = sum + cardPoints[j] - cardPoints[ptr];
+            if(max < sum) max = sum;
+            
+            ptr--;
+            j--;
+            loop++;
         }
         
-        int maxScore = totalSum - windowSum;
+        return max;
         
-        for(int i = n -k; i < n; i++){
-            windowSum += cardPoints[i] - cardPoints[i - (n - k)];
-            maxScore = Math.max(maxScore, totalSum - windowSum);
-        }
-        
-        
-        return maxScore;
+      
     }
 }
