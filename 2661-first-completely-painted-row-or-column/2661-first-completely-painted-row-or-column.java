75be91ccc0d614bd81635dfa9d89328309fa3f0a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-04-30 14:52:57 +0530

    Time: 18 ms (100.00%), Space: 63.1 MB (100.00%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -1,24 +1,38 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-       int m = mat.length, n = mat[0].length;
-        int[][] loc = new int[m * n][2];
-        for(int i =0; i < m; i++){
-            for(int j = 0; j< n; ++j){
-                loc[mat[i][j]-1][0] = i;
-                loc[mat[i][j]-1][1] = j;
+        int rows = mat.length;
+        int cols = mat[0].length;
+        
+        // Store the locations of all the integers in mat
+        int[][] integerLocations = new int[rows * cols][2];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int integer = mat[i][j];
+                integerLocations[integer-1][0] = i; // Row index
+                integerLocations[integer-1][1] = j; // Column index
             }
         }
         
-        int[] r = new int[m];
-        int[] c = new int[n];
-        
-        for(int x =0; x < arr.length; ++x){
-            int[] currentLoc = loc[arr[x]-1];
-            int i = currentLoc[0];
-            int j = currentLoc[1];
-            if(++r[i] == n || ++c[j] == m) return x;
+        // Count the number of painted cells in each row and column
+        int[] paintedInRow = new int[rows];
+        int[] paintedInCol = new int[cols];
+        for (int i = 0; i < arr.length; ++i) {
+            int integer = arr[i];
+            int[] location = integerLocations[integer-1];
+            int row = location[0];
+            int col = location[1];
+            
+            // Increment the count of painted cells in the current row and column
+            paintedInRow[row]++;
+            paintedInCol[col]++;
+            
+            // If any row or column is fully painted, return the current index
+            if (paintedInRow[row] == cols || paintedInCol[col] == rows) {
+                return i;
+            }
         }
         
+        // No row or column is fully painted
         return -1;
     }
 }
