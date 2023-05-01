aa8df3f7e1665c35437d5415f83d1cafb237f77a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-05-01 21:59:42 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (83.61%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,12 @@
+ 
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        int sum = 0;
+        for(int i =1; i<salary.length-1;i++){
+            sum += salary[i];
+        }
+        double out = (double) sum / (salary.length-2);
+        return out;
+    }
+}
