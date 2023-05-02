2540d00230f290f7b817e7a31882d3ea3c49aaa7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-05-02 10:56:52 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (38.04%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int negativeCount  =0;
+        int negativeCount = 0; // Initialize the count of negative numbers to 0
         for(int num : nums) {
-          
             if(num == 0) {
-                return 0;
-            } else if(num<0){
-                negativeCount++;
+                return 0; // If the element is 0, the product is 0
+            } else if(num < 0) {
+                negativeCount++; // Increment the count if the element is negative
             }
-            
-            
         }
         
-        if(negativeCount % 2 == 0) return 1;
-        else return -1;
+        if(negativeCount % 2 == 0) {
+            return 1; // Return 1 if the number of negative numbers is even
+        } else {
+            return -1; // Return -1 if the number of negative numbers is odd
+        }
     }
 }
