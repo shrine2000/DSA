39f3a5df128d17e98bec72c85483e93ef6fc43d7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-05-28 01:03:10 +0530

    Time: 178 ms (38.04%), Space: 17.1 MB (49.14%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t:
+            return ""
+        
+        required = Counter(t)
+        
+        left = 0
+        right = 0
+        formed = 0
+        minLength = float('inf')
+        startIndex = 0
+        
+        while right < len(s):
+            
+            if s[right] in required:
+                required[s[right]] -= 1
+                if required[s[right]] >= 0:
+                    formed += 1
+            
+            
+            while formed == len(t):
+                if right - left + 1 < minLength:
+                    minLength = right - left + 1
+                    startIndex = left
+                    
+                if s[left] in required:
+                    required[s[left]] += 1
+                    if required[s[left]] > 0:
+                        formed -=1
+                        
+                left += 1
+                
+            right += 1
+            
+        if minLength == float('inf'):
+            return ""
+        
+        return s[startIndex:startIndex + minLength]
+                
