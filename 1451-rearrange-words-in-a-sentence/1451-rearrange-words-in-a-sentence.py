c3fe1bbd2953f9583105a8fa5f93f1a199f7f89f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-02 19:24:01 +0530

    Time: 52 ms (67.18%), Space: 18.6 MB (36.29%) - LeetHub

---
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrangeWords(self, text: str) -> str:
+        words = text.split()
+        words[0] = words[0].lower()  # Convert the first word to lowercase
+        words.sort(key=len)  # Sort the words based on their lengths
+        
+        # Capitalize the first letter of the first word
+        words[0] = words[0][0].upper() + words[0][1:]
+        
+        return ' '.join(words)
