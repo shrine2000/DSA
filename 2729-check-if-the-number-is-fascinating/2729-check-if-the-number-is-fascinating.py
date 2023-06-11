decb47b3ab61b3f8ce848b51527573716f683bb6
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-11 21:25:47 +0530

    Time: 46 ms (42.86%), Space: 16.4 MB (14.29%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
@@ -1,9 +1,5 @@
 class Solution:
     def isFascinating(self, n: int) -> bool:
-        original_digits = str(n)
-        
-        concatenated_digits = original_digits + str(2 * n) + str(3 * n)
-    
-        unique_digits = set(concatenated_digits)
-
-        return len(unique_digits) == 9 == len(concatenated_digits) and '0' not in unique_digits
+        new_digit = str(n) + str( 2 * n) + str(3 * n)
+        unique_digits = set(new_digit)
+        return len(unique_digits) == 9 and len(new_digit) ==  9 and '0' not in unique_digits
