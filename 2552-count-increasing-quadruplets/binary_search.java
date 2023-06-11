3807f22a6c5dd9913536fab73a1019e03c13aceb
Shrine Sabu <shrine02sabu@gmail.com>
2023-06-11 21:32:03 +0530

    TODO

---
+++ 2552-count-increasing-quadruplets/binary_search.java
@@ -0,0 +1,16 @@
+
+class binary_search{
+
+    // For all the combination of the j and k, find how many i and l possibility.
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+
+
+}
--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
@@ -3,3 +3,5 @@ class Solution:
         new_digit = str(n) + str( 2 * n) + str(3 * n)
         unique_digits = set(new_digit)
         return len(unique_digits) == 9 and len(new_digit) ==  9 and '0' not in unique_digits
+    
+# TODO check - https://leetcode.com/contest/biweekly-contest-106/submissions/detail/968072317/
