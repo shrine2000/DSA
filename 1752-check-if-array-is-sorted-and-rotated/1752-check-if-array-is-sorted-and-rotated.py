4dcdffd48afc92c3d1925189ee8af4587e0cdc79
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-12 18:32:11 +0530

    Time: 47 ms (60.00%), Space: 16.4 MB (20.37%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,29 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        left = 0
+        right = n - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            elif nums[mid] < nums[right]:
+                right = mid
+            else:
+                if nums[right-1] > nums[right]:
+                    left = right
+                    break
+                right -= 1
+                
+        smallest = left
+        
+        check = (smallest + 1) % n
+        
+        while check != smallest:
+            if nums[check] < nums[check - 1]:
+                return False
+            check = (check + 1) % n
+            
+        return True
