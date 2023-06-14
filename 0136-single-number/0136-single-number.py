27ddac527b3ad7b87a1684c971fa341267ef9e95
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-14 19:47:03 +0530

    Time: 150 ms (37.21%), Space: 19.4 MB (13.98%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap = {}
+        
+        for num in nums:
+            if num in hashmap:
+                 hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+            
+            
+        for num, count in hashmap.items():
+            if count == 1:
+                return num
+            
