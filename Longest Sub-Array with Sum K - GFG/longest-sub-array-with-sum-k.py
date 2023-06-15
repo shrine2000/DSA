de8f6a786d735b93a26cded3efb38a67637c1508
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-15 09:10:26 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+    #TODO 
+    
+   def lenOfLongSubarr(self, a, n, k):
+        n = len(a) 
+    
+        preSumMap = {}
+        Sum = 0
+        maxLen = 0
+        for i in range(n):
+             
+            Sum += a[i]
+    
+           
+            if Sum == k:
+                maxLen = max(maxLen, i + 1)
+    
+            
+            rem = Sum - k
+    
+            
+            if rem in preSumMap:
+                length = i - preSumMap[rem]
+                maxLen = max(maxLen, length)
+    
+            if Sum not in preSumMap:
+                preSumMap[Sum] = i
+    
+        return maxLen
+    
+     
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    
+    n, k = map(int , input().split())
+    arr = list(map(int,input().strip().split()))
+    ob = Solution()
+    print(ob.lenOfLongSubarr(arr, n, k))
+
+
+
+
+# } Driver Code Ends
