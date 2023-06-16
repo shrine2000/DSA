269d2aeafac1235cc45144208d1da8e1bb5b7b24
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-16 21:26:51 +0530

    Time: 369 ms (98.71%), Space: 18.6 MB (45.95%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        if not heightMap or not heightMap[0]:
+            return 0
+        rows, cols = len(heightMap), len(heightMap[0])
+        trappedWater = 0
+        visited = [[False] * cols for _ in range(rows)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        pq = []
+        
+        for r in range(rows):
+            heapq.heappush(pq, (heightMap[r][0], r, 0))
+            heapq.heappush(pq, (heightMap[r][cols - 1], r, cols - 1))
+            visited[r][0] = True
+            visited[r][cols - 1] = True
+            
+        for c in range(cols):
+            heapq.heappush(pq, (heightMap[0][c], 0, c))
+            heapq.heappush(pq, (heightMap[rows - 1][c], rows - 1, c))
+            visited[0][c] = True
+            visited[rows - 1][c] = True
+            
+        while pq:
+            height, row, col = heapq.heappop(pq)
+            
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                if 0 <= new_row < rows and 0 <= new_col < cols and not visited[new_row][new_col]:
+                    trappedWater += max(0, height - heightMap[new_row][new_col])
+                    heapq.heappush(pq, (max(height, heightMap[new_row][new_col]), new_row, new_col))
+                    visited[new_row][new_col] = True
+        
+        return trappedWater
