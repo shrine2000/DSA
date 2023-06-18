8931651c782b58495b5597ed550bda481598fc09
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-18 14:49:11 +0530

    Time: 100 ms (71.43%), Space: 16.4 MB (28.57%) - LeetHub

---
+++ 2739-total-distance-traveled/2739-total-distance-traveled.py
@@ -0,0 +1,21 @@
+class Solution:
+    def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+        if mainTank < 5:
+            return mainTank * 10
+        
+        
+        ans = 0
+        
+        
+        while mainTank > 0:
+            if mainTank >= 5:
+                ans += 50
+                mainTank -= 5
+                if additionalTank > 0:
+                    mainTank += 1
+                    additionalTank -= 1
+                    continue
+            ans += mainTank * 10
+            mainTank = 0
+        return ans
+        
