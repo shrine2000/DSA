de34ccc77960f52facfc98637f5207e7f708ccb8
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-18 16:08:50 +0530

    Time: 51 ms (41.99%), Space: 16.4 MB (31.06%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,4 +1,7 @@
 class Solution:
+    
+    # https://www.geeksforgeeks.org/find-n-distinct-integers-with-zero-sum/
+    
     def sumZero(self, n: int) -> List[int]:
         out = []
         for i in range(1, n // 2 + 1):
