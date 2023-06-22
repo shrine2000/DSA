0d554ee8e2e6be8169bc4995301d8035828508c6
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-22 17:54:17 +0530

    Time: 105 ms (21.78%), Space: 17.8 MB (24.72%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+        start, end = -1, -1
+
+        while left <= right and nums[left] <= target <= nums[right]:
+            if left == right or nums[left] == nums[right]:
+                if nums[left] == target:
+                    start = left
+                break
+
+            pos = left + ((target - nums[left]) * (right - left)) // (nums[right] - nums[left])
+            if nums[pos] == target:
+                start = pos
+                right = pos - 1
+            elif nums[pos] < target:
+                left = pos + 1
+            else:
+                right = pos - 1
+
+        left, right = 0, len(nums) - 1
+        while left <= right and nums[left] <= target <= nums[right]:
+            if left == right or nums[left] == nums[right]:
+                if nums[left] == target:
+                    end = right
+                break
+
+            pos = left + ((target - nums[left]) * (right - left)) // (nums[right] - nums[left])
+            if nums[pos] == target:
+                end = pos
+                left = pos + 1
+            elif nums[pos] < target:
+                left = pos + 1
+            else:
+                right = pos - 1
+
+        return [start, end]
