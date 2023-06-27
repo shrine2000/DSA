f0e3a46c3e381d60af205a2f5e98b147709ed088
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-06-27 10:32:04 +0530

    Time: 705 ms (24.74%), Space: 16.4 MB (66.65%) - LeetHub

---
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.py
@@ -0,0 +1,16 @@
+import math
+from typing import List
+
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        count = 0
+        
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if self.isCoprime(int(str(nums[i])[0]), int(str(nums[j])[-1])):
+                    count += 1
+                    
+        return count        
+        
+    def isCoprime(self, x, y) -> bool:
+        return math.gcd(x, y) == 1
