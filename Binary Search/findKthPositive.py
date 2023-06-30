ed92fe2c8b5233c8d5486f01ac11f88f0b5bb89d
Shrine Sabu <shrine02sabu@gmail.com>
2023-06-30 23:32:26 +0530

    findKthPositive

---
+++ Binary Search/findKthPositive.py
@@ -0,0 +1,19 @@
+def findKthPositive(arr, k):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        missing_count = arr[mid] - (mid + 1)
+
+        if missing_count < k:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left + k
+
+arr = [2, 3, 4, 7, 11]
+k = 5
+result = findKthPositive(arr, k)
+print(result)  # Output: 6
