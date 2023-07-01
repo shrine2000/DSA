efb1bcf307e206941834efa85211e99240487093
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-01 13:52:49 +0530

    Time: 3357 ms (15.67%), Space: 30.9 MB (60.32%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -1,21 +1,20 @@
 class Solution:
-    def minimumTime(self, time, totalTrips):
-        low = 1
-        high = 2**63 - 100000
-        ans = high
-        while low <= high:
-            mid = (low + high) // 2
-            if self.valid(time, mid, totalTrips):
-                ans = min(ans, mid)
-                high = mid - 1
-            else:
-                low = mid + 1
-        return ans
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        left = 1
+        right = 10**14
 
-    def valid(self, time, t, totalTrips):
-        temp = 0
-        for x in time:
-            temp += t // x
-            if temp >= totalTrips:
-                return True
-        return temp >= totalTrips
+        while left < right:
+            mid = left + (right - left) // 2
+            if self.isPossible(mid, time, totalTrips):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
+    def isPossible(self, mid: int, time: List[int], totalTrips: int) -> bool:
+        total = 0
+        for t in time:
+            total += mid // t
+
+        return total >= totalTrips
