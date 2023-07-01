baa8bcdee7023a268263a850c5ca6d6d6761cbcc
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-01 13:51:44 +0530

    Time: 371 ms (13.61%), Space: 56.2 MB (51.77%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,31 +1,26 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        long low = 1;
-        long high = Long.MAX_VALUE - 100000;
-        long ans = high;
+        long left = 1;
+        long right = (long) 1e14;
 
-        while (low <= high) {
-            long mid = (low + high) / 2;
-            if (isValid(time, mid, totalTrips)) {
-                ans = Math.min(ans, mid);
-                high = mid - 1;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (isPossible(mid, time, totalTrips)) {
+                right = mid;
             } else {
-                low = mid + 1;
+                left = mid + 1;
             }
         }
-        
-        return ans;
+
+        return left;
     }
 
-    private boolean isValid(int[] time, long t, int totalTrips) {
-        long temp = 0;
-        for (int x : time) {
-            temp += t / x;
-            if (temp >= totalTrips) {
-                return true;
-            }
+    private boolean isPossible(long mid, int[] time, int totalTrips) {
+        long total = 0;
+        for (int t : time) {
+            total += mid / t;
         }
-        
-        return temp >= totalTrips;
+
+        return total >= totalTrips;
     }
 }
