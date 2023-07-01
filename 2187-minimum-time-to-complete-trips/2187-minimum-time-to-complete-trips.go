57f038514aafb62789c9c655449a8b7ee5136072
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-01 14:10:57 +0530

    Time: 374 ms (47.73%), Space: 8.8 MB (52.27%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.go
@@ -0,0 +1,34 @@
+func minimumTime(time []int, totalTrips int) int64 {
+	left := min(time)
+	right := min(time) * totalTrips
+
+	for left < right {
+		mid := left + (right-left)/2
+		if isPossible(mid, time, totalTrips) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return int64(left)
+}
+
+func isPossible(mid int, time []int, totalTrips int) bool {
+	total := 0
+	for _, t := range time {
+		total += mid / t
+	}
+
+	return total >= totalTrips
+}
+
+func min(time []int) int {
+	minVal := time[0]
+	for _, t := range time {
+		if t < minVal {
+			minVal = t
+		}
+	}
+	return minVal
+}
