c2249f0300740fe0054264a9160dfc30f8f28b0d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-03 23:48:59 +0530

    Time: 61 ms (8.10%), Space: 16.3 MB (65.57%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        absent_count = 0
+        late_count = 0
+
+        for record in s:
+            if record == 'A':
+                absent_count += 1
+                late_count = 0
+                if absent_count > 1:
+                    return False
+            elif record == 'L':
+                late_count += 1
+                if late_count > 2:
+                    return False
+            else:
+                late_count = 0
+
+        return True
