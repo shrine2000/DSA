88d41943c27804ea95c5b83f8d212ac2f61dc146
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-08 23:16:55 +0530

    Time: 1726 ms (44.56%), Space: 28.2 MB (79.65%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maximumTastiness(self, price: List[int], k: int) -> int:
+        
+        def isPossible(mid, price, k):
+            prev = price[0]
+            count = 0
+
+            for i in range(1, len(price)):
+                if price[i] - prev >= mid:
+                    prev = price[i]
+                    count += 1
+
+            count += 1
+            return count >= k
+
+        
+        
+        price.sort()
+        
+        left, right = 0, price[-1] - price[0]
+        ans = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isPossible(mid, price, k):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return ans
