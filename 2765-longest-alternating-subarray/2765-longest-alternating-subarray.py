1b9535eb947ba514837c89179adb1e04b3b6db45
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-09 15:06:29 +0530

    Time: 156 ms (42.86%), Space: 16.4 MB (42.86%) - LeetHub

---
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def alternatingSubarray(self, nums: List[int]) -> int:
+        ans = -1
+        
+        for i in range(len(nums)):
+            p = -1
+            for j in range(i + 1, len(nums)):
+                if (nums[j] - nums[j-1]) != p * -1:
+                    break
+                else:
+                    ans = max(ans, j - i + 1)
+                    p *= -1
+        
+        return ans
