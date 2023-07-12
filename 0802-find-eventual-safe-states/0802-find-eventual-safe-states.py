de99566b50cafcdda9d51228059523fca334bc25
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-12 10:42:46 +0530

    Time: 645 ms (90.30%), Space: 23.7 MB (75.79%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,25 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        visited = [0] * n
+        safe_nodes = []
+        
+        def is_safe(node):
+            if visited[node] != 0:
+                return visited[node] == 2
+            
+            visited[node] = 1
+            
+            for neighbor in graph[node]:
+                if not is_safe(neighbor):
+                    return False
+                
+            visited[node] = 2
+            
+            return True
+        
+        for node in range(n):
+            if is_safe(node):
+                safe_nodes.append(node)
+                
+        return safe_nodes
