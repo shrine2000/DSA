5e85ed4614fae18bc475240f34eea5cea158b7e0
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-18 01:52:31 +0530

    Time: 49 ms (63.48%), Space: 16.5 MB (30.77%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        components = path.split('/')
+        
+        for component in components:
+            if component == '.' or component == '':
+                continue
+            elif component == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(component)
+        
+        simplified_path = '/' + '/'.join(stack)
+        return simplified_path
