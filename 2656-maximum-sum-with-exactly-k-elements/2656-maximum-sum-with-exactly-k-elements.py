5eb82e0e7630a7ad9ac581e48ada7b06513b78b9
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-07-30 12:50:35 +0530

    Time: 181 ms (76.71%), Space: 16.3 MB (49.24%) - LeetHub

---
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution:
+    def maximizeSum(self, nums: List[int], k: int) -> int:
+        pq = []
+        res = 0
+        
+        for num in nums:
+            heapq.heappush(pq, -num) 
+            
+        while pq and k > 0:
+            top_element = -heapq.heappop(pq)  
+            res += top_element
+            
+            next_element = top_element + 1
+            heapq.heappush(pq, -next_element)   
+            
+            k -= 1
+            
+        return res
