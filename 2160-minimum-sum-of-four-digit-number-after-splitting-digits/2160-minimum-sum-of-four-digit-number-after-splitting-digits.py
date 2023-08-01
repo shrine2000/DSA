3c85b867f906aee76776cf8bf864b7d368b0cd00
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-01 08:05:09 +0530

    Time: 36 ms (94.16%), Space: 16.4 MB (27.37%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -0,0 +1,22 @@
+import queue
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        pq = queue.PriorityQueue()
+        digits = [int(digit) for digit in str(num)]
+        for digit in digits:
+            pq.put(digit)
+
+            
+        # 2932 => 2239
+        # 4009 => 0049
+        
+        smallest1 = pq.get()
+        smallest2 = pq.get()
+        largest1 = pq.get()
+        largest2 = pq.get()
+        
+        new1 = smallest1 * 10 + largest1  
+        new2 = smallest2 * 10 + largest2   
+        
+        return new1 + new2
