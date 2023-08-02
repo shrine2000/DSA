2b4e6c0531ffa18dfd531824337e549d3f7521f5
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-02 21:57:03 +0530

    Time: 142 ms (99.67%), Space: 16.7 MB (77.67%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        arr = sorted(boxTypes, key=lambda x: x[1], reverse=True)
+        
+        total_units = 0
+        boxes_added = 0
+        
+        """
+        [5, 10] = 50
+        [3, 9]  = 27
+        [4, 7]  = 28
+        [2, 5]  = 10
+        
+        """
+        
+        for box_count, unit_per_box in arr:
+            boxes_to_add = min(truckSize - boxes_added, box_count)
+            total_units += boxes_to_add * unit_per_box
+            boxes_added += boxes_to_add
+            if boxes_added == truckSize:
+                break
+            
+        return total_units
+        
+        
