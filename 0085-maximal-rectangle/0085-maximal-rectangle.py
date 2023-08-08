d7789f6fd51bf210668479f0a01d8a6d07ea979b
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-08 14:16:32 +0530

    Time: 279 ms (77.64%), Space: 17.8 MB (15.11%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,38 +1,50 @@
-from typing import List
-
 class Solution:
-    def maxHistogram(self, heights):
-        heights.append(0)
-        n = len(heights)
+    def largestRectangleArea(self, heights):
         stack = []
-        res = 0 
-        i = 0
-        
-        while i < n:
-            if not stack or heights[i] >= heights[stack[-1]]:
-                stack.append(i)
-                i += 1
+        n = len(heights)
+        nsl = [-1] * n
+        nsr = [n] * n
+        area = []
+
+        for i in range(n):  # Nearest Smallest Element to Left
+            while stack and stack[-1][0] >= heights[i]:
+                stack.pop()
+            if not stack:
+                nsl[i] = -1
             else:
-                top = stack.pop()
-                res = max(res, heights[top] * (i if not stack else i - stack[-1] - 1))
-        return res
-    
+                nsl[i] = stack[-1][1]
+            stack.append((heights[i], i))
+
+        stack = []
+        for i in range(n - 1, -1, -1):  # Nearest Smallest Element to Right
+            while stack and stack[-1][0] >= heights[i]:
+                stack.pop()
+            if not stack:
+                nsr[i] = n
+            else:
+                nsr[i] = stack[-1][1]
+            stack.append((heights[i], i))
+
+        ans = 0
+        for i in range(n):
+            width = nsr[i] - nsl[i] - 1
+            area.append(heights[i] * width)
+            ans = max(ans, area[i])
+
+        return ans
+
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        if not matrix or not matrix[0]:
-            return 0
-        r, c = len(matrix), len(matrix[0])
         area = 0
-        
-        height = [0] * c
-        
-        for i in range(r):
-            for j in range(c):
+        m = len(matrix)
+        n = len(matrix[0])
+        heights = [0] * n
+
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == '1':
-                    height[j] += 1
+                    heights[j] += 1
                 else:
-                    height[j] = 0
-                
-            area = max(area, self.maxHistogram(height))
-        
-        
+                    heights[j] = 0
+            area = max(area, self.largestRectangleArea(heights))
+
         return area
