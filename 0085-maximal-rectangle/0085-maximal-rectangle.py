d17c6160ae1e940d1bc62e6aeb13cbbbe07dd012
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-08 09:31:58 +0530

    Time: 246 ms (95.02%), Space: 17.6 MB (93.50%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,22 +1,38 @@
+from typing import List
+
 class Solution:
+    def maxHistogram(self, heights):
+        heights.append(0)
+        n = len(heights)
+        stack = []
+        res = 0 
+        i = 0
+        
+        while i < n:
+            if not stack or heights[i] >= heights[stack[-1]]:
+                stack.append(i)
+                i += 1
+            else:
+                top = stack.pop()
+                res = max(res, heights[top] * (i if not stack else i - stack[-1] - 1))
+        return res
+    
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix or not matrix[0]:
             return 0
-
-        rows, cols = len(matrix), len(matrix[0])
-        heights = [0] * (cols + 1)
-        max_area = 0
-
-        for row in matrix:
-            for col in range(cols):
-                heights[col] = heights[col] + 1 if row[col] == "1" else 0
-
-            stack = [-1]
-            for col in range(cols + 1):
-                while heights[col] < heights[stack[-1]]:
-                    height = heights[stack.pop()]
-                    width = col - stack[-1] - 1
-                    max_area = max(max_area, height * width)
-                stack.append(col)
-
-        return max_area
+        r, c = len(matrix), len(matrix[0])
+        area = 0
+        
+        height = [0] * c
+        
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] == '1':
+                    height[j] += 1
+                else:
+                    height[j] = 0
+                
+            area = max(area, self.maxHistogram(height))
+        
+        
+        return area
