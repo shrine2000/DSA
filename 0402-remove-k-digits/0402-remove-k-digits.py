905816e3796c7558a3c46dc397836f71386f0ff7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-09 08:51:50 +0530

    Time: 75 ms (75.41%), Space: 18 MB (28.02%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,37 +1,18 @@
 class Solution:
-    
-    def nearest_greater_left(self, arr, k):
-        n = len(arr)
-        result = [-1] * n  
-
-        stack = []  
-        for i in range(n):
-            while stack and arr[stack[-1]] <= arr[i]:
-                stack.pop()
-
-            if stack:
-                result[i] = arr[stack[-1]]
-
-            stack.append(i)
-
-        return result
-
     def removeKdigits(self, num: str, k: int) -> str:
-        res = self.nearest_greater_left(num, k)
-        out = []
-        for i in range(len(num)):
-            while out and k > 0 and out[-1] > num[i]:
-                out.pop()
+        stack = []
+        
+        for digit in num:
+            while stack and k > 0 and stack[-1] > digit:
+                stack.pop()
                 k -= 1
-            out.append(num[i])
-
+            stack.append(digit)
+            
         while k > 0:
-            out.pop()
+            stack.pop()
             k -= 1
             
-        result = "".join(out).lstrip("0")
-
-        if not result:
-            return "0"
-
-        return result
+        result = "".join(stack).lstrip("0")
+        
+        return result if result else "0"
+        
