d7d94e822feafec83b6d3eba90f9d1993d95847f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-12 14:03:49 +0530

    Time: 15 ms (37.50%), Space: 6.8 MB (7.69%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.go
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.go
@@ -1,33 +1,37 @@
-impl Solution {
-    pub fn insert_greatest_common_divisors(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        fn gcd(mut a: i32, mut b: i32) -> i32 {
-            while b != 0 {
-                let temp = b;
-                b = a % b;
-                a = temp;
-            }
-            a
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func insertGreatestCommonDivisors(head *ListNode) *ListNode {
+    // Helper function to calculate GCD
+    gcd := func(a, b int) int {
+        for b != 0 {
+            a, b = b, a%b
         }
-
-        let mut dummy = Some(Box::new(ListNode::new(0)));
-        let mut prev = &mut dummy;
-
-        let mut current = head;
-        while let Some(mut curr_node) = current.take() {
-            if let Some(next_node) = curr_node.next.take() {
-                let gcd_val = gcd(curr_node.val, next_node.val);
-                let new_node = Some(Box::new(ListNode::new(gcd_val)));
-
-                prev.as_mut().unwrap().next = Some(curr_node);
-                prev.as_mut().unwrap().next.as_mut().unwrap().next = new_node;
-                prev = &mut prev.as_mut().unwrap().next.as_mut().unwrap().next;
-                current = Some(next_node);
-            } else {
-                prev.as_mut().unwrap().next = Some(curr_node);
-                break;
-            }
-        }
-
-        dummy.unwrap().next
+        return a
     }
+
+    dummy := &ListNode{Val: 0, Next: head}
+    prev := dummy
+
+    current := head
+    for current != nil && current.Next != nil {
+        currNode := current
+        nextNode := current.Next
+
+        gcdVal := gcd(currNode.Val, nextNode.Val)
+        newNode := &ListNode{Val: gcdVal}
+
+        prev.Next = currNode
+        currNode.Next = newNode
+        newNode.Next = nextNode
+
+        prev = newNode
+        current = nextNode
+    }
+
+    return dummy.Next
 }
