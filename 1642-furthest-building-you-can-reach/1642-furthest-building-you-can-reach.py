b415b7daa8711225585e148235b09a5b1e6ac211
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-15 00:50:29 +0530

    Time: 479 ms (93.92%), Space: 30.9 MB (69.59%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,33 +1,26 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        def check_reachability(heights, bricks, ladders, mid):
-            diff = []  # Store height differences greater than previous ones
-            
-            # Calculate and store height differences for the first 'mid' buildings
-            for i in range(1, mid + 1):
-                if heights[i] > heights[i - 1]:
-                    diff.append(heights[i] - heights[i - 1])
-            
-            # Sort the height differences in decreasing order
-            # The largest differences can be covered using ladders
-            diff.sort(reverse=True)
-            
-            l = len(diff)
-            for i in range(ladders, l):
-                if diff[i] > bricks:
-                    return False  # Check if remaining differences can be covered using bricks
-                bricks -= diff[i]
-            return True
-        
+        ladders_used = []
         n = len(heights)
-        lo, hi = 0, n - 1
         
-        # Binary search on the answer (furthest building index)
-        while lo < hi:
-            mid = (lo + hi + 1) // 2
-            if check_reachability(heights, bricks, ladders, mid):
-                lo = mid  # If true, binary search on the right half
+        for i in range(n - 1):
+            climb = heights[i+1] - heights[i]
+            
+            if climb <= 0:
+                continue
+                
+            if len(ladders_used) < ladders:
+                heapq.heappush(ladders_used, climb)
+            elif ladders_used and ladders_used[0] < climb:
+                ladder_height = heapq.heappop(ladders_used)
+                bricks -= ladder_height
+                
+                heapq.heappush(ladders_used, climb)
+                
             else:
-                hi = mid - 1  # If false, binary search on the left half
-        
-        return lo  # Return the index of the furthest reachable building
+                bricks -= climb
+                
+            if bricks < 0:
+                return i
+            
+        return n - 1
