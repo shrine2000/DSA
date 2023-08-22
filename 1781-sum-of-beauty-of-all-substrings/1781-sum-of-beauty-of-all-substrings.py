502c17e6f2db3e7ce0758bf4d089d5a682dd0d19
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-22 10:15:00 +0530

    Time: 457 ms (100.00%), Space: 16.4 MB (38.50%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,18 +1,29 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        n = len(s)
-        prefix_freq = [Counter()]
-        
-        for i in range(1, n + 1):
-            prefix_freq.append(prefix_freq[i - 1] + Counter(s[i - 1]))
-        
         total_beauty = 0
+        n = len(s)
+        normalized_string = [ord(c) - ord('a') for c in s]
         
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                substring_freq = prefix_freq[j] - prefix_freq[i]
-                max_freq = max(substring_freq.values(), default=0)
-                min_freq = min(substring_freq.values(), default=0)
-                total_beauty += max_freq - min_freq
+        for i in range(n - 1):
+            char_count = [0] * 26  # 
+            char_count[normalized_string[i]] = 1
+            occurrences = [0] * n   
+            occurrences[1] = 1
+            max_occurrence = min_occurrence = 1
+            
+            for char in normalized_string[i + 1:]:
+                occurrences[char_count[char]] -= 1
+                char_count[char] += 1
+                occurrences[char_count[char]] += 1
+                
+                if char_count[char] > max_occurrence:
+                    max_occurrence = char_count[char]
+                    
+                if char_count[char] == 1:
+                    min_occurrence = 1
+                elif occurrences[min_occurrence] == 0:
+                    min_occurrence += 1
+                
+                total_beauty += max_occurrence - min_occurrence
         
         return total_beauty
