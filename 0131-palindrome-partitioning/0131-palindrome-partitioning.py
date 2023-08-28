716ff4b924523ddf96b894de9b141c40b8a50016
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-08-28 11:56:11 +0530

    Time: 553 ms (73.70%), Space: 31.1 MB (89.35%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def backtrack(idx, ns, path, res):
+            if idx == len(ns):
+                res.append(path[:])   
+                return
+            
+            for i in range(idx, len(ns)):
+                if isPalin(ns, idx, i):
+                    path.append(ns[idx:i+1])
+                    backtrack(i + 1, ns, path, res)   
+                    path.pop()   
+            
+        def isPalin(s, start, end):
+            return s[start:end+1] == s[start:end+1][::-1]
+        
+        res = []
+        path = []
+        
+        backtrack(0, s, path, res)
+        
+        return res
