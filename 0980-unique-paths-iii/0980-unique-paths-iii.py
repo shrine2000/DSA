f94e49705ea4a8f90674febebc593cc79b00ad19
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-09-02 22:05:10 +0530

    Time: 51 ms (91.29%), Space: 16.2 MB (90.96%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        def dfs(i, j, s, t):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == -1:
+                return 0
+            
+            if grid[i][j] == 2:
+                return 1 if s == t else 0
+
+                
+            grid[i][j] = -1
+            
+            path = (
+             dfs(i + 1, j, s + 1, t) + dfs(i, j + 1, s + 1, t) + dfs(i - 1, j, s + 1, t) + dfs(i, j - 1, s + 1, t)
+            )
+            
+            grid[i][j] = 0
+            
+            return path
+
+        sr, sc, t = 0, 0, 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    sr, sc = i, j
+                if grid[i][j] != -1:
+                    t += 1
+
+        return dfs(sr, sc, 1, t)
