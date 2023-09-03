9c6ce674da671350b74685a66305b94a0c4c77b7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-09-03 13:03:00 +0530

    Time: 35 ms (100.00%), Space: 2.1 MB (100.00%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.rs
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.rs
@@ -1,8 +1,26 @@
-class Solution:
-    def countSymmetricIntegers(self, low: int, high: int) -> int:
-        c = 0
-        for i in range(low, high + 1):  
-            s = str(i)
-            if len(s) % 2 == 0 and sum(map(int, s[:len(s)//2])) == sum(map(int, s[len(s)//2:])):
-                c += 1
-        return c
+impl Solution {
+    pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
+        fn is_symmetric(n: i32) -> bool {
+            let s = n.to_string();
+            let len = s.len();
+            if len % 2 != 0 {
+                return false; 
+            }
+            
+            let half_len = len / 2;
+            let left_sum: i32 = s.chars().take(half_len).map(|c| c.to_digit(10).unwrap() as i32).sum();
+            let right_sum: i32 = s.chars().skip(half_len).map(|c| c.to_digit(10).unwrap() as i32).sum();
+            
+            left_sum == right_sum
+        }
+        
+        let mut count = 0;
+        for num in low..=high {
+            if is_symmetric(num) {
+                count += 1;
+            }
+        }
+        
+        count
+    }
+}
