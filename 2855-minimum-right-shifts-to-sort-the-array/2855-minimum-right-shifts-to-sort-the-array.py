4c4edd53712642b1662178ea14edee64cce8622c
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-09-17 12:13:15 +0530

    Time: 54 ms (57.14%), Space: 16.2 MB (57.14%) - LeetHub

---
+++ 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumRightShifts(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] < nums[i - 1]:
+                nums = nums[i:] + nums[:i]
+                if nums == sorted(nums):
+                    return n - i
+                else:
+                    return -1
+        
+        return 0
