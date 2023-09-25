3daf0659973f1572195aff16120f260bb4efe065
Shrine Sabu <shrine02sabu@gmail.com>
2023-09-25 19:48:48 +0530

    DP

---
+++ 0062-unique-paths-ii/brute_force.py
@@ -0,0 +1,20 @@
+
+from typing import List
+class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(r, c):
+            if r >= m or c >= n or grid[r][c] == 1:
+                return 0
+
+            if r == m - 1 or c == n - 1:
+                return 1
+
+            down = dfs(r + 1, c)
+            right = dfs(r, c + 1)
+
+            return down + right
+
+        return dfs(0, 0)
