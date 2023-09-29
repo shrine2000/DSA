7b2d8e6ea28822bca666017e0a59a2b6bfca39c4
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-09-29 21:25:55 +0530

    Time: 75 ms (24.07%), Space: 17.4 MB (63.50%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,25 +1,16 @@
-from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         if not triangle:
             return 0
         
-        dp = [0] * len(triangle)
-        dp[0] = triangle[0][0]
+        n = len(triangle)
+        dp = [[0] * (i + 1) for i in range(n)]  
         
-        for i in range(1, len(triangle)):
-            # Initialize the rightmost element in the row
-            dp[i] = dp[i - 1] + triangle[i][-1]
-            
-            # Update the elements from right to left within the row
-            for j in range(len(triangle[i]) - 2, 0, -1):
-                dp[j] = triangle[i][j] + min(dp[j], dp[j - 1])
+        dp[-1] = triangle[-1]
+        
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1])
                 
-            # Update the leftmost element in the row
-            dp[0] += triangle[i][0]
-        
-        # Find the minimum path sum at the top of the triangle
-        min_path_sum = min(dp)
-        
-        return min_path_sum
+        return dp[0][0]
