74561c341fe8dc4506f7edb6f23b79c266f89d1b
Shrine Sabu <shrine02sabu@gmail.com>
2023-09-30 13:00:29 +0530

    DP

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,9 +1,11 @@
+from typing import List
+
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         r, c = len(matrix), len(matrix[0])
 
         dp = [[0] * c for _ in range(r)]
-        dp[0] = matrix[0]  
+        dp[0] = matrix[0]
 
         for i in range(1, r):
             for j in range(c):
@@ -15,3 +17,8 @@ class Solution:
                     dp[i][j] = matrix[i][j] + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1])
 
         return min(dp[-1])
+
+if __name__ == "__main__":
+    solution = Solution()
+    matrix = [[2,1,3],[6,5,4],[7,8,9]]
+    print(solution.minFallingPathSum(matrix))
