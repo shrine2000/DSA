a8d269178c447bc5bd743b9127e6723291bc2a3c
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-10-09 12:47:14 +0530

    DP

--- Dynamic Programming/518. Coin Change II /recursion.py
+++ Dynamic Programming/518. Coin Change II /recursion.py
@@ -1,31 +1,37 @@
-def count_ways_to_make_change_util(coins, index, target, dp):
-    if index == 0:
-        return 1 if target & coins[0] == 0 else 0
-
-    if dp[index][target] != -1:
-        return dp[index][target]
-
-    not_taken = count_ways_to_make_change_util(coins, index - 1, target, dp)
-
-    taken = 0
-    if coins[index] <= target:
-        taken = count_ways_to_make_change_util(coins, index, target - coins[index], dp)
-
-    dp[index][target] = not_taken + taken
-    return dp[index][target]
+from typing import List
 
 
-def count_ways_to_make_change(coins, target):
-    n = len(coins)
-    dp = [[-1 for _ in range(target + 1)] for _ in range(n)]
-    return count_ways_to_make_change_util(coins, n - 1, target, dp)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        return self._change_recursive(amount, coins, 0)
+
+    def _change_recursive(self, amount: int, coins: List[int], index: int) -> int:
+        # Base cases:
+        # If the amount is 0, there's one way to make change (no coins used).
+        if amount == 0:
+            return 1
+        # If the amount is negative or we've exhausted all coins, there are no ways to make change.
+        if amount < 0 or index >= len(coins):
+            return 0
+
+        # Recursive calls:
+        # For each coin, we have two choices:
+        # 1. Pick the current coin and make a recursive call with reduced amount and same coin.
+        # 2. Do not pick the current coin and make a recursive call with the same amount and next coin.
+        ways_with_current_coin = self._change_recursive(amount - coins[index], coins, index)
+        ways_without_current_coin = self._change_recursive(amount, coins, index + 1)
+
+        # The total ways is the sum of the ways with and without the current coin.
+        return ways_with_current_coin + ways_without_current_coin
 
 
+# Example usage
 def main():
-    coins = [1, 2, 3]
-    target = 4
-    num_ways = count_ways_to_make_change(coins, target)
-    print("The total number of ways is", num_ways)
+    solution = Solution()
+    amount = 5
+    coins = [1, 2, 5]
+    result = solution.change(amount, coins)
+    print("The number of ways to make change is:", result)
 
 
 if __name__ == "__main__":
