b15d61c30185235d37d79f3bb02955497241377d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-10-21 18:42:44 +0530

    Time: 1208 ms (12.29%), Space: 16.7 MB (61.92%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,31 +1,30 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        num_words = len(words)
-        graph = [[] for _ in range(num_words)]   
-        longest_chain_lengths = [0] * num_words   
-        word_positions = {word: position for position, word in enumerate(words)}
-
-        def find_longest_chain(position):
-            if longest_chain_lengths[position] > 0:
-                return longest_chain_lengths[position]
-
-            longest_chain_lengths[position] = 1
-
-            for neighbor in graph[position]:
-                longest_chain_lengths[position] = max(longest_chain_lengths[position], find_longest_chain(neighbor) + 1)
-
-            return longest_chain_lengths[position]
-
-        for current_position in range(num_words):
-            current_word = words[current_position]
-            for char_position in range(len(current_word)):
-                potential_word = current_word[:char_position] + current_word[char_position + 1:]
-                if potential_word in word_positions:
-                    graph[word_positions[potential_word]].append(current_position)
-
-        max_chain_length = 0
-        for position in range(num_words):
-            max_chain_length = max(max_chain_length, find_longest_chain(position))
-
-        return max_chain_length
+        n = len(words)
+        words.sort(key=lambda x : len(x))
         
+        longestChainLengths = [1] * n
+        maxChainLength = 1
+        
+        def is_predecessor(prev, curr):
+            M, N = len(prev), len(curr)
+            
+            if M >= N or N - M != 1:
+                return False
+            
+            i, j = 0, 0
+            
+            while i < M and j < N:
+                if prev[i] == curr[j]:
+                    i += 1
+                j += 1
+                
+            return i == M
+        
+        for i in range(n):
+            for j in range(i):
+                if is_predecessor(words[j], words[i]):
+                    longestChainLengths[i] = max(longestChainLengths[i], longestChainLengths[j] + 1)
+                    maxChainLength = max(maxChainLength, longestChainLengths[i])
+                    
+        return maxChainLength
