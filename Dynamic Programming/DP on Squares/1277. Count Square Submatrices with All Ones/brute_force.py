11c2b7f38d7c5d147adeb1390690fcb68901bfa5
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-10-26 06:45:46 +0530

    DP

---
+++ Dynamic Programming/DP on Squares/1277. Count Square Submatrices with All Ones/brute_force.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        if not matrix:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+
+
+
+
+
+if __name__ == "__main__":
+    matrix = [
+        [0, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 1, 1, 1]
+    ]
+    sol = Solution
+    print(sol.countSquares(matrix=matrix))
+
+
