36ade77fbc902fe06a9e376e1e8ef147dcd42acb
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-10-28 19:06:47 +0530

    DP

---
+++ Dynamic Programming/ DP on Subsequences/Subset sum equal to target /brute_force.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def isSubsetSum(self, N: int, arr: List[int], target_sum: int) -> bool:
+        def helper(arr: List[int], k: int, n: int) -> bool:
+            if n < 0 or k < 0:
+                return False
+
+            if k == 0:
+                return True
+
+            pick: bool = helper(arr, k - arr[n], n - 1)
+            not_pick: bool = helper(arr, k, n - 1)
+
+            return pick or not_pick
+
+        return helper(arr, target_sum, N - 1)
+
+if __name__ == "__main__":
+    arr: List[int] = [3, 34, 4, 12, 5, 2]
+    target_sum: int = 9
+    print(Solution().isSubsetSum(len(arr), arr, target_sum))
---
+++ Dynamic Programming/DP on Grids/2088. Count Fertile Pyramids in a Land/brute_force.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple
+
+class Solution:
+    def countPyramids(self, grid: List[List[int]]) -> int:
+        def is_valid_pyramid(apex: Tuple[int, int], height: int) -> bool:
+            r, c = apex
+            
+            if (r, c, height) in ph:
+                return ph[(r, c, height)]
+            
+            for i in range(r, r + height):
+                for j in range(c - (i - r), c + (i - r) + 1):
+                    if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                        return False
+            ph[(r, c, height)] = True
+            return True
+
+        def is_valid_inverse_pyramid(apex: Tuple[int, int], height: int) -> bool:
+            r, c = apex
+
+            if (r, c, height) in iph:
+                return iph[(r, c, height)]
+
+            for i in range(r - height + 1, r + 1):
+                for j in range(c - (r - i), c + (r - i) + 1):
+                    if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                        return False
+            iph[(r, c, height)] = True
+            return True
+
+        m, n = len(grid), len(grid[0])
+        total_plots = 0
+        ph, iph = {}, {}
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    for height in range(2, m - i + 1):
+                        if is_valid_pyramid((i, j), height):
+                            total_plots += 1
+
+                    for height in range(2, i + 2):
+                        if is_valid_inverse_pyramid((i, j), height):
+                            total_plots += 1
+
+        return total_plots
