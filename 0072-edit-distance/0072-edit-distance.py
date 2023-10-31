1269f71a42ea89af717d3ee09b4aa13f94372054
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-10-31 22:23:13 +0530

    Time: 125 ms (54.97%), Space: 20 MB (30.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,28 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        len1, len2 = len(word1), len(word2)
+        m, n = len(word1), len(word2)
         
-        queue = [(0, 0)] 
-        visited = set()
-        distance = 0
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
         
-        while queue:
-            next_level = []
-            while queue:
-                i, j = queue.pop()
-                if (i, j) in visited:
-                    continue
-                while i < len1 and j < len2 and word1[i] == word2[j]:
-                    i += 1
-                    j += 1
-                if i == len1 and j == len2:
-                    return distance
-                if (i, j + 1) not in visited:
-                    next_level.append((i, j + 1))
-                if (i + 1, j) not in visited:
-                    next_level.append((i + 1, j))
-                if (i + 1, j + 1) not in visited:
-                    next_level.append((i + 1, j + 1))
-                visited.add((i, j))
-            distance += 1
-            queue = next_level
+        for j in range(n+1):
+            dp[0][j] = j
+            
+        for i in range(m+1):
+            dp[i][0] = i
+            
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j -1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 +  min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
+                
+        return dp[m][n]
