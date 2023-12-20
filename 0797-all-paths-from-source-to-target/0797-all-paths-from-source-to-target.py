4b50decadc9d3d5e52abca71d236625e48ebe921
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2023-12-20 14:51:52 +0530

    Time: 79 ms (92.37%), Space: 19.3 MB (5.20%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,17 +1,20 @@
+from typing import List
+
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
         paths = []
-        
-        def dfs(node, path):
+        queue = [(0, [0])]  
+
+        while queue:
+            node, path = queue.pop(0)   
             if node == n - 1:
-                paths.append(path[:])
-                return 
-            
-            for next_node in graph[node]:
-                path.append(next_node)
-                dfs(next_node, path)
-                path.pop()
-            
-        dfs(0, [0])
+                paths.append(path)  
+                continue
+
+            for neighbor in graph[node]:
+                new_path = list(path)  
+                new_path.append(neighbor) 
+                queue.append((neighbor, new_path))   
+
         return paths
