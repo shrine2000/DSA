93a4182105931d1bcc9f59e7c96fd43f9ad2bd13
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-01-04 18:30:24 +0530

    Time: 43 ms (82.73%), Space: 19.1 MB (36.35%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def traverse(root, prev):
+            if root is None:
+                return True
+            
+            if not traverse(root.left, prev):
+                return False
+            
+            if root.val <= prev[0]:
+                return False
+            
+            prev[0] = root.val
+            
+            return traverse(root.right, prev)
+        
+        prev = [float('-inf')]
+        
+        return traverse(root, prev)
