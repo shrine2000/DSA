57cfbe3de1881634b64b83b0c0f6560fe5d8d323
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-01-10 08:47:06 +0530

    Time: 1105 ms (72.50%), Space: 57 MB (9.17%) - LeetHub

---
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def cycleLengthQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        # Define a method to find the lowest common ancestor (LCA)
+        def find_LCA(a, b):
+            while a != b:
+                if a > b:
+                    a //= 2
+                else:
+                    b //= 2
+            return a
+
+        # Define a method to calculate the depth of a node
+        def calculate_depth(node):
+            depth = 0
+            while node > 1:
+                node //= 2
+                depth += 1
+            return depth
+
+        def find_distance(a, b):
+            lca = find_LCA(a, b)
+            return calculate_depth(a) + calculate_depth(b) - 2 * calculate_depth(lca)
+
+        def cycle_length(n, queries):
+            result = []
+            for query in queries:
+                a, b = query
+                distance = find_distance(a, b)
+                result.append(distance + 1)  # Adding 1 to the distance to account for the edge
+            return result
+
+        return cycle_length(n, queries)
