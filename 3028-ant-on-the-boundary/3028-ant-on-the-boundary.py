5a941e5ef80bbba73b46724869d39b82ee2c44e7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-02-04 12:48:28 +0530

    Time: 42 ms (100.00%), Space: 16.6 MB (100.00%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        count = 0
+        position = 0
+
+        for num in nums:
+            position += num
+            if position == 0:
+                count += 1
+
+        return count
