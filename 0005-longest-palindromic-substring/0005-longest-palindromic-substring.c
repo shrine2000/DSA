4a38c01b8055cffd7541a8cf67a2407139d78bb3
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-02-19 08:49:05 +0530

    Time: 1168 ms (5.68%), Space: 6 MB (82.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -1,14 +1,36 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        def isPalindrome(string):
-            return string == string[::-1]
-        
-        longest = ""
-        
-        for i in range(len(s)):
-            for j in range(i + 1, len(s) + 1):
-                substr = s[i:j]
-                if isPalindrome(substr) and len(substr) > len(longest):
-                    longest = substr
-        
-        return longest
+int isPalindrome(char* s, int start, int end) {
+    while (start < end) {
+        if (s[start] != s[end]) {
+            return 0;
+        }
+        start++;
+        end--;
+    }
+    return 1;
+}
+
+char* longestPalindrome(char* s) {
+    int maxLength = 0;
+    int start = 0;
+    int end = 0;
+    int len = strlen(s);
+    
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (isPalindrome(s, i, j)) {
+                int currLength = j - i + 1;
+                if (currLength > maxLength) {
+                    maxLength = currLength;
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+    }
+    
+    char* result = (char*)malloc((maxLength + 1) * sizeof(char));
+    strncpy(result, s + start, maxLength);
+    result[maxLength] = '\0';
+    
+    return result;
+}
