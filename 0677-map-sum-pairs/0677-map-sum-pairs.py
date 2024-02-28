3965ecc3623e37fa825510400c4503098753d35e
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-02-28 08:30:29 +0530

    Time: 35 ms (76.87%), Space: 16.6 MB (61.04%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.py
+++ 0677-map-sum-pairs/0677-map-sum-pairs.py
@@ -1,13 +1,40 @@
-class MapSum:
+class TrieNode:
     def __init__(self):
         self.children = {}
+        self.value = 0
+        
+        
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, key: str, val: int) -> None:
-        self.children[key] = val
+        node = self.root
+        for k in key:
+            if not k in node.children:
+                node.children[k] = TrieNode()
+            node = node.children[k]
+        node.value = val
+        
 
     def sum(self, prefix: str) -> int:
-        count = 0
-        for k, v in self.children.items():
-            if k.startswith(prefix):
-                count += v
-        return count
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return 0
+            node = node.children[char]
+        return self._dfs(node)
+    
+    def _dfs(self, node):
+        total = node.value
+        for child in node.children.values():
+            total += self._dfs(child)
+        return total
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
