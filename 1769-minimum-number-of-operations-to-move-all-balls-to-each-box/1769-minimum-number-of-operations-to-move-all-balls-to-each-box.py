675e49bd2674523e19257c6d34b5d55b414e8363
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-03-02 14:01:56 +0530

    Time: 49 ms (95.49%), Space: 16.9 MB (68.62%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -2,19 +2,16 @@ from typing import List
 
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        n = len(boxes)
-        boxes = [int(each) for each in boxes]
-        ans = [0 for _ in range(n)]
-        prevBall = boxes[0]
-        prevMove = 0
-        for i in range(1, n):
-            prevMove += prevBall 
-            ans[i] += prevMove
-            prevBall += boxes[i]
-        prevBall = boxes[-1]
-        prevMove = 0
-        for i in range(n-2, -1, -1):
-            prevMove += prevBall
-            ans[i] += prevMove
-            prevBall += boxes[i]
-        return ans
+        boxes = [1 if box == "1" else 0 for box in boxes]
+        distance = sum([i for i in range(len(boxes)) if boxes[i] == 1])
+        left = boxes[0]
+        right = sum(boxes) - boxes[0]
+        result = []
+        result.append(distance)
+        for i in range(1, len(boxes)):
+            distance += left
+            distance -= right
+            left += boxes[i]
+            right -= boxes[i]
+            result.append(distance)
+        return result
