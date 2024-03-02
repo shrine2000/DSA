c501a81683ac3ce5622639e10e1a2e2f0a102f92
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-03-02 13:57:26 +0530

    Time: 50 ms (94.56%), Space: 16.9 MB (68.62%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -3,16 +3,18 @@ from typing import List
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        ans = [0] * n
-        left_balls = 0
-        right_balls = boxes.count("1")
-        left_sum = 0
-        right_sum = sum(i for i, box in enumerate(boxes) if box == "1")
-        for i, box in enumerate(boxes):
-            ans[i] = left_sum + right_sum
-            if box == "1":
-                left_balls += 1
-                right_balls -= 1
-            left_sum += left_balls
-            right_sum -= right_balls
+        boxes = [int(each) for each in boxes]
+        ans = [0 for _ in range(n)]
+        prevBall = boxes[0]
+        prevMove = 0
+        for i in range(1, n):
+            prevMove += prevBall 
+            ans[i] += prevMove
+            prevBall += boxes[i]
+        prevBall = boxes[-1]
+        prevMove = 0
+        for i in range(n-2, -1, -1):
+            prevMove += prevBall
+            ans[i] += prevMove
+            prevBall += boxes[i]
         return ans
