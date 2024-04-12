24ffb4325c036712f6cfbde1569c476072383010
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-04-12 23:03:01 +0530

    Time: 336 ms (88.24%) | Memory: 31.9 MB (50.27%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,23 +1,16 @@
 class Solution:
-     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        priority_queue = []
-        for num in nums:
-            heapq.heappush(priority_queue, num)
-        
-        max_len = 1
-        current_len = 1
-        prev_num = heapq.heappop(priority_queue)
-        
-        while priority_queue:
-            num = heapq.heappop(priority_queue)
-            if num == prev_num + 1:
-                current_len += 1
-            elif num != prev_num:
-                max_len = max(max_len, current_len)
-                current_len = 1
-            prev_num = num
-        
-        return max(max_len, current_len)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        count = 0
+        prev = None
+        max_len = 0
+
+        for num in sorted(nums_set):
+            if prev is None or num == prev + 1:
+                count += 1
+            else:
+                count = 1  
+            max_len = max(max_len, count)
+            prev = num  
+
+        return max_len
