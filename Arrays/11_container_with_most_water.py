705faa1567f75537c3320ef1560bccea0f8416d7
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-06-01 22:21:49 +0530

    dsa

---
+++ Arrays/11_container_with_most_water.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+       pass
+
+
+if __name__ == "__main__":
+    height = [2,3,4,5,18,17,6]
+    assert Solution().maxArea(height) == 17
