0ca2f2d8571e1ada2cafb1b3697dd1bd36c31abf
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-06-04 09:27:24 +0530

    Time: 970 ms (5.05%) | Memory: 205.1 MB (14.74%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,7 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_global = max_current = nums[0]
-        for num in nums[1:]:
-            max_current = max(num, num + max_current)
-            max_global = max(max_global, max_current)
-        return max_global
+        memo = {}
+        def solve(nums, pick, idx):
+            if idx >= len(nums):
+                return 0 if pick else float("-inf")
+            if (pick, idx) in memo:
+                return memo[(pick, idx)]
+            if pick:
+                result =  max(0, nums[idx] + solve(nums, True, idx + 1))
+            else:
+                result = max( nums[idx] + solve(nums, True, idx + 1), solve(nums, False, idx + 1))
+            memo[(pick, idx)] = result
+            return result
+        return solve(nums, False, 0)
