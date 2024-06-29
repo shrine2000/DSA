ee8f846d2062d95af10f91893d82b0dd16a422f8
Shrine Sabu <shrine02sabu@gmail.com>
2024-06-29 15:55:20 +0530

    dsa

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -17,22 +17,20 @@ class Node:
 
 # https://www.youtube.com/watch?v=FrD3_PXwhj0
 class Solution:
-    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
-        if not root:
+    class Solution:
+        def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+            if not root:
+                return root
+            current = root
+            while current.left:
+                next_level = current.left
+                while current:
+                    current.left.next = current.right
+                    if current.next:
+                        current.right.next = current.next.left
+                    current = current.next
+                current = next_level
             return root
-        queue = deque()
-        queue.append(root)
-        while queue:
-            level_size = len(queue)
-            for i in range(level_size):
-                node = queue.popleft()
-                if i < level_size - 1:
-                    node.next = queue[0]
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-        return root
 
 
 if __name__ == "__main__":
