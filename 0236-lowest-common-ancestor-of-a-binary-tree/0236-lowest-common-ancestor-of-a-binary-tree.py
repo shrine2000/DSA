84430c9c09bc8e53aabe5b4114beb9bcc10357bd
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-07-03 21:19:29 +0530

    dsa

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -5,15 +5,18 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
         if not root or root == p or root == q:
             return root
-        
+
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-        
+
         if left and right:
             return root
-        
+
         return left if left else right
--- Line Sweep Algorithms /57_insert_interval.py
+++ Line Sweep Algorithms /57_insert_interval.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def insert(
+    def brute_force_insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
         # Initialize the list to hold merged intervals
@@ -24,13 +24,48 @@ class Solution:
 
         return merged_intervals
 
+    def optimized_insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        # Initialize the list to hold merged intervals
+        merged_intervals = []
+
+        i, n = 0, len(intervals)
+
+        # Add all intervals that come before the new interval
+        while i < n and intervals[i][1] < newInterval[0]:
+            merged_intervals.append(intervals[i])
+            i += 1
+
+        # Merge all overlapping intervals with the new interval
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # Add the merged new interval
+        merged_intervals.append(newInterval)
+
+        # Add all intervals that come after the new interval
+        while i < n:
+            merged_intervals.append(intervals[i])
+            i += 1
+
+        return merged_intervals
+
 
 # Time Complexity: O((N+1) log (N+1)) due to the sorting step
 # Space Complexity: O(N) for storing the merged intervals
 
 if __name__ == "__main__":
     sol = Solution()
+    intervals = [[1, 3], [6, 9]]
+    newInterval = [2, 5]
+    # result = sol.brute_force_insert(intervals, newInterval)
+    result = sol.optimized_insert(intervals, newInterval)
+    assert result == [[1, 5], [6, 9]]
+
     intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
     newInterval = [4, 8]
-    result = sol.insert(intervals, newInterval)
+    result = sol.optimized_insert(intervals, newInterval)
     assert result == [[1, 2], [3, 10], [12, 16]]
