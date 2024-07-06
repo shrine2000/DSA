27edd6a1bddcdc011e02bdc9c65d943a772a51ee
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-07-06 23:06:53 +0530

    Time: 41 ms (27.80%), Space: 16.9 MB (13.26%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -8,21 +8,20 @@ class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-
+        
         column_table = defaultdict(list)
-        min_column = max_column = 0
+        min_col = max_col = 0        
         queue = deque([(root, 0, 0)])
         while queue:
             node, row, column = queue.popleft()
             if node:
                 column_table[column].append((row, node.val))
-                min_column = min(min_column, column)
-                max_column = max(max_column, column)
+                min_col = min(min_col, column)
+                max_col = max(max_col, column)
                 queue.append((node.left, row + 1, column - 1))
                 queue.append((node.right, row + 1, column + 1))
-
         result = []
-        for column in range(min_column, max_column + 1):
-            result.append([val for row, val in sorted(column_table[column])])
+        for col in range(min_col, max_col + 1):
+            result.append([v for r, v in sorted(column_table[col])])
         return result
                 
