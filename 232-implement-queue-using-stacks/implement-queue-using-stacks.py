dfba50d4c9128b30887d062b65ce0b8b9f249160
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-08-07 09:16:37 +0530

    Time: 36 ms (48.37%) | Memory: 16.5 MB (53.46%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,28 +1,30 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack1 = []
-        self.stack2 = []
+        self.input_stack = []
+        self.output_stack = []
+        
 
     def push(self, x: int) -> None:
-        self.stack1.append(x)
+        self.input_stack.append(x)
 
     def pop(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        return self.stack2.pop()
+        if not self.output_stack:
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
+        return self.output_stack.pop()
+        
 
     def peek(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        return self.stack2[-1]
+        if not self.output_stack:
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
+        return self.output_stack[-1]
         
 
     def empty(self) -> bool:
-        return not self.stack1 and not self.stack2
-    
+        return not self.input_stack and not self.output_stack
+        
 
 
 # Your MyQueue object will be instantiated and called as such:
