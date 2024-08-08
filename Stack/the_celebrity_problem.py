5d83311f47c9364da7b03d3e8ba84fddd18d4d9d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-08-08 22:24:15 +0530

    dsa

---
+++ Stack/the_celebrity_problem.py
@@ -0,0 +1,23 @@
+def find_celebrity_brute_force(M):
+    N = len(M)
+    
+    def is_celebrity(c):
+        for j in range(N):
+            if j != c and M[c][j] == 1:
+                return False
+        for i in range(N):
+            if i != c and M[i][c] == 0:
+                return False
+        return True
+    
+    for person in range(N):
+        if is_celebrity(person):
+            return person
+    
+    return -1
+
+MATRIX1 = [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 1, 0]]
+MATRIX2 = [[0, 0, 1, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 1, 0]]
+
+print(find_celebrity_brute_force(MATRIX1))  # Output: 2
+print(find_celebrity_brute_force(MATRIX2))  # Output: -1
