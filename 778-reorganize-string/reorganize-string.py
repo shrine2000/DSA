9ba65c0516a9614ea0a060cb3ac81560e38906f9
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-09-03 22:16:04 +0530

    Time: 39 ms (44.51%) | Memory: 16.5 MB (72.85%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -4,15 +4,6 @@ class Solution:
         heap = []
         for char, freq in char_freq.items():
             heapq.heappush(heap, (-freq, char))
-            
-        """
-        {
-        "v" : 3,
-        "l" : 1,
-        "o" : 1,
-        }
-        
-        """
         prev_char, prev_freq = "", 0
         result_str = []
         while heap:
@@ -25,7 +16,6 @@ class Solution:
             prev_char = char
             prev_freq = freq + 1
         res = "".join(result_str)
-        print(res)
         if len(res) == len(s):
             return res
         return ""
