6c32689271e9cece82810b09fb4c28333f667f30
Shrine Sabu <shrine02sabu@gmail.com>
2024-09-06 21:33:29 +0530

    dsa

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,3 +1,7 @@
+from collections import defaultdict, deque
+from typing import List
+
+
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         graph = defaultdict(list)
@@ -24,3 +28,22 @@ class Solution:
                     queue.append(neighbor)
         
         return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    recipes1 = ["bread"]
+    ingredients1 = [["yeast", "flour"]]
+    supplies1 = ["yeast", "flour", "corn"]
+    print(solution.findAllRecipes(recipes1, ingredients1, supplies1))  # Output: ["bread"]
+
+    recipes2 = ["bread", "sandwich"]
+    ingredients2 = [["yeast", "flour"], ["bread", "meat"]]
+    supplies2 = ["yeast", "flour", "meat"]
+    print(solution.findAllRecipes(recipes2, ingredients2, supplies2))  # Output: ["bread", "sandwich"]
+
+    recipes3 = ["bread", "sandwich", "burger"]
+    ingredients3 = [["yeast", "flour"], ["bread", "meat"], ["sandwich", "meat", "bread"]]
+    supplies3 = ["yeast", "flour", "meat"]
+    print(solution.findAllRecipes(recipes3, ingredients3, supplies3))
