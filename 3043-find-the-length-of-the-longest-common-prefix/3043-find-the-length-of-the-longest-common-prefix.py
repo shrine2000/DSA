ed085d94944d4456f1430cb57daf29b7071bd266
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-10-02 19:40:32 +0530

    Time: 944 ms (45.98%), Space: 32.3 MB (32.24%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,46 @@
+from typing import List
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def longest_common_prefix(self, word: str) -> int:
+        node = self.root
+        prefix_length = 0
+        
+        for char in word:
+            if char in node.children:
+                prefix_length += 1
+                node = node.children[char]
+            else:
+                break
+
+        return prefix_length
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        trie = Trie()
+        
+        for num in arr1:
+            trie.insert(str(num))
+        
+        max_length = 0
+        for num in arr2:
+            max_length = max(max_length, trie.longest_common_prefix(str(num)))
+        
+        return max_length
+
+ 
