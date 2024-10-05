4769158c4257dea0b3313f68d4576d0e7788d2ab
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2024-10-06 00:31:02 +0530

    Time: 1116 ms (29.72%) | Memory: 17.1 MB (98.58%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,28 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.arr = defaultdict(int)
+        
+
+    def book(self, start: int, end: int) -> bool:
+        
+        self.arr[start] += 1
+        self.arr[end] -= 1
+        overlap = 0
+        for time in sorted(self.arr.keys()):
+            overlap += self.arr[time]   
+            if overlap >= 3:
+                self.arr[start] -= 1
+                self.arr[end] += 1
+                if self.arr[start] == 0:
+                    del self.arr[start]
+                if self.arr[end] == 0:
+                    del self.arr[end]
+                return False
+        
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
