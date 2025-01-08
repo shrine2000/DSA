37b8ad9737e4216f1d1e969b8b42076aa2d126dc
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-01-08 23:24:30 +0530

    Time: 3 ms (79.04%) | Memory: 18 MB (7.90%) - LeetSync

---
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        covered_points = set()
+        
+        for start, end in nums:
+            for point in range(start, end + 1): 
+                covered_points.add(point)
+        
+        return len(covered_points)
