5ecdea5a2698086e77d84c3ce1b7609b5858fcd0
Shrine Sabu <shrine02sabu@gmail.com>
2025-01-27 00:26:00 +0530

    dsa

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         self.good = 0
@@ -18,3 +18,36 @@ class Solution:
 
         dfs(root, root.val)
         return self.good
+
+if __name__ =="__main__":
+    # Tree structure:
+    #     3
+    #    / \
+    #   1   4
+    #  /   / \
+    # 3   1   5
+
+    root = TreeNode(3)
+    root.left = TreeNode(1)
+    root.right = TreeNode(4)
+    root.left.left = TreeNode(3)
+    root.right.left = TreeNode(1)
+    root.right.right = TreeNode(5)
+
+    solution = Solution()
+    print(solution.goodNodes(root))  # Expected output: 4
+
+    # Tree structure:
+    #     3
+    #    /
+    #   3
+    #  / \
+    # 4   2
+
+    root = TreeNode(3)
+    root.left = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(2)
+
+    solution = Solution()
+    print(solution.goodNodes(root))  # Expected output: 3
