23d46691ef20342e8ee80235724ef45e3b741148
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-04-27 11:04:46 +0530

    Time: 7 ms (58.57%) | Memory: 17.7 MB (74.08%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
@@ -1,16 +1,11 @@
 class Solution:
-    def _is_valid(self, nums):
-        if not nums or len(nums) != 3:
-            return False
-        return bool(nums[1] / 2 == (nums[0] + nums[2]))
-
     def countSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
         count = 0
-        for i in range(n):
-            for j in range(i, n):
-                if self._is_valid(nums[i:j+1]):
-                    count += 1
-        return count
 
-        
+        for i in range(n - 2):  
+            first, middle, third = nums[i], nums[i + 1], nums[i + 2]
+            if middle == 2 * (first + third):
+                count += 1
+
+        return count
