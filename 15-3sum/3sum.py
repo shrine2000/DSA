f64426ee3a01c411dd99f4b1b6249d074f68dce5
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-04-29 01:25:21 +0530

    Time: 575 ms (60.66%) | Memory: 20.8 MB (23.10%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,24 +3,30 @@ class Solution:
         nums.sort()
         n = len(nums)
         triplets = []
-        for i in range(n - 2):
+
+        for i in range (n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             left, right = i + 1, n - 1
+
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
+                total = nums[left] + nums[right] + nums[i]
 
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif total < 0:
+                if total < 0:
                     left += 1
+                elif total > 0:
+                    right -= 1
                 else:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
                     right -= 1
-
+                
         return triplets
