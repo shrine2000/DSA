862acea570bfab888812617aede15090706a2574
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-05-17 17:15:56 +0530

    Time: 130 ms (68.34%) | Memory: 78.7 MB (27.63%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,16 +2,18 @@ class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next: Node | None = None
-        self.prev: Node | None = None
-
+        self.next: Optional["Node"] = None
+        self.prev: Optional["Node"] = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity: int = capacity
         self.cache: dict[int, Node] = {}  # hold keys and their corresponding nodes
         self.head: Node = Node(0, 0)  # dummy head
         self.tail: Node = Node(0, 0)  # dummy tail
+
+        # initialize doubly linked list
         self.head.next = (
             self.tail
         )  # prev of dummy head points to NULL and next point to dummy tail
@@ -20,12 +22,14 @@ class LRUCache:
         )  # prev of dummy tail points to head, next point to NULL
 
     def _remove(self, node: Node):
+        """remove node from dll"""
         prev = node.prev
         nxt = node.next
         prev.next = nxt
         nxt.prev = prev
 
-    def _add(self, node: Node):
+    def _add_to_tail(self, node: Node):
+        """add node right before the tail (most recently used)"""
         prev = self.tail.prev
         prev.next = node
         node.prev = prev
@@ -35,8 +39,8 @@ class LRUCache:
     def get(self, key):
         if key in self.cache:
             node = self.cache[key]
-            self._remove(node)
-            self._add(node)
+            self._remove(node)  # move the accessed node to tail (MRU)
+            self._add_to_tail(node)
             return node.value
         return -1
 
@@ -50,7 +54,7 @@ class LRUCache:
             del self.cache[lru.key]
         # add new node or update the existing node
         new_node = Node(key, value)
-        self._add(new_node)
+        self._add_to_tail(new_node)
         self.cache[key] = new_node
 
 
