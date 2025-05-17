4bb7ec32643ea4ec98b30dc0abb1362537b5cde8
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-05-17 21:11:21 +0530

    Time: 152 ms (42.75%) | Memory: 78.7 MB (27.63%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,64 +1,55 @@
 class Node:
     def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next: Optional["Node"] = None
-        self.prev: Optional["Node"] = None
+        self.key=key
+        self.value=value
+        self.prev=None
+        self.next=None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity: int = capacity
-        self.cache: dict[int, Node] = {}  # hold keys and their corresponding nodes
-        self.head: Node = Node(0, 0)  # dummy head
-        self.tail: Node = Node(0, 0)  # dummy tail
+        self.cache ={}
+        self.capacity=capacity
+        self.tail=Node(0, 0)
+        self.head=Node(0, 0)
 
-        # initialize doubly linked list
-        self.head.next = (
-            self.tail
-        )  # prev of dummy head points to NULL and next point to dummy tail
-        self.tail.prev = (
-            self.head
-        )  # prev of dummy tail points to head, next point to NULL
+        self.head.next=self.tail
+        self.tail.prev=self.head
 
-    def _remove(self, node: Node):
-        """remove node from dll"""
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    def _add_to_tail(self, node: Node):
-        """add node right before the tail (most recently used)"""
-        prev = self.tail.prev
-        prev.next = node
-        node.prev = prev
-        node.next = self.tail
-        self.tail.prev = node
-
-    def get(self, key):
+    def _remove(self,node):
+        _prev=node.prev
+        _next=node.next
+        _prev.next=_next
+        _next.prev=_prev
+  
+    def _add_to_tail(self,node):
+        tail_prev=self.tail.prev
+        tail_prev.next=node
+        node.prev=tail_prev
+        node.next=self.tail
+        self.tail.prev=node
+    
+    def get(self, key: int) -> int:
         if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)  # move the accessed node to tail (MRU)
+            node=self.cache[key]
+            self._remove(node)
             self._add_to_tail(node)
             return node.value
         return -1
+        
 
-    def put(self, key: int, value: int):
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self._remove(self.cache[key])
+
         elif len(self.cache) >= self.capacity:
-            # remove the least recently used node which is right after the head
-            lru = self.head.next
+            lru=self.head.next
             self._remove(lru)
             del self.cache[lru.key]
-        # add new node or update the existing node
-        new_node = Node(key, value)
+        new_node=Node(key,value)
         self._add_to_tail(new_node)
-        self.cache[key] = new_node
+        self.cache[key]=new_node
 
+        
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+ 
