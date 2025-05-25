37abfe142400ed2571c660bc22209016cbbd005a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-05-25 15:39:43 +0530

    dsa

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -7,35 +7,36 @@ class Node:
         self.random = random
 """
 
+from typing import Optional
+
+from LinkedList.node import Node
+
+
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
         if not head:
             return None
 
-        curr=head
+        curr = head
         while curr:
-            new_node= Node(curr.val)
+            new_node = Node(curr.value)
             new_node.next = curr.next
             curr.next = new_node
-            curr=new_node.next
+            curr = new_node.next
 
-        curr=head
+        curr = head
         while curr:
             if curr.random:
-                curr.next.random=curr.random.next
-            curr=curr.next.next
+                curr.next.random = curr.random.next
+            curr = curr.next.next
 
-        curr=head
-        pseudo_head=Node(0)
-        copy_curr=pseudo_head
+        curr = head
+        pseudo_head = Node(0)
+        copy_curr = pseudo_head
         while curr:
-            copy=curr.next
-            curr.next=copy.next
-            copy_curr.next=copy
-            copy_curr=copy
-            curr=curr.next
+            copy = curr.next
+            curr.next = copy.next
+            copy_curr.next = copy
+            copy_curr = copy
+            curr = curr.next
         return pseudo_head.next
-
-        
-
-
