27797893f511f8eff23f69563d717dcb0f802e9b
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-06-02 23:56:37 +0530

    Time: 38 ms (22.65%) | Memory: 23.5 MB (19.30%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_surplus, current_surplus = 0, 0
+        idx = 0
+
+        for i in range(len(gas)):
+            delta = gas[i] - cost[i]
+            total_surplus += delta
+            current_surplus += delta
+        
+            if current_surplus < 0:
+                current_surplus = 0
+                idx = i + 1
+        return idx if total_surplus >= 0 else -1
