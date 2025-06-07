730bb9b8ae4b8a493661f0bae3d2351697c623d8
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-06-07 21:21:55 +0530

    Time: 61 ms (20.32%) | Memory: 34.6 MB (19.38%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        N = len(s)
+        result = []
+        def is_valid(string):
+            if not string:
+                return False
+            return string.lower() == string.lower()[::-1]
+        def backtrack(start, path):
+            if start == N:
+                result.append(path[:])
+                return
+            for end in range(start + 1, N + 1):
+                substring = s[start:end]
+                if is_valid(substring):
+                    path.append(substring)              
+                    backtrack(end, path)              
+                    path.pop()                 
+        backtrack(0, [])
+        return result
+            
+
+
+            
