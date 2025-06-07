0236ffdde6490c523ad3c4a9444382288a608eed
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-06-07 20:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (37.21%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
+        result: List[List[int]] = []
+
+        def backtrack(idx, current_combination):
+            result.append(current_combination[:])
+            
+            for i in range(idx, N):
+                if i > idx and nums[i] == nums[i - 1]:
+                    continue
+
+                current_combination.append(nums[i])
+                backtrack(i + 1, current_combination)
+                current_combination.pop()
+
+        nums.sort()
+        backtrack(0, [])
+        return result
