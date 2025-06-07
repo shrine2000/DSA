e4b9d1854501ed0996e40974183d763ace9ac77f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-06-07 20:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.76%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
+        result: List[List[int]] = []
+
+        def backtrack(idx, current_combination):
+            result.append(current_combination[:])
+            
+            for i in range(idx, N):
+                current_combination.append(nums[i])
+                backtrack(i + 1, current_combination)
+                current_combination.pop()
+        backtrack(0, [])
+        return result
