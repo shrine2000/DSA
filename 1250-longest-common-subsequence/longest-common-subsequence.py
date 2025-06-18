3c0c98e66dd139825adb7166b43c6c71e7dd63f4
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-06-18 09:26:05 +0530

    Time: 465 ms (58.08%) | Memory: 42.9 MB (58.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,13 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        M, N = len(text1), len(text2)
-        @cache
-        def recursion(i, j):
-            if i == M or j == N:
-                return 0
-            
-            if text1[i] == text2[j]:
-                return 1 + recursion(i + 1, j + 1)
-            
-            return max(recursion(i + 1, j), recursion(i, j+ 1))
-        return recursion(0, 0)
+        N, M = len(text1), len(text2)
+        dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+        for i in range(1, N +1):
+            for j in range(1, M+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j - 1])
+        return dp[N][M]
