525d3a1dee8d31b7ab448eef13c04c714f50a0e1
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-07-14 00:03:37 +0530

    Time: 4 ms (36.10%) | Memory: 23.4 MB (38.66%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,23 +8,28 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head
-
+        slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        mid = slow
         prev = None
-        current = mid
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        first, second = head, prev
-        while second and second.next:
-            temp1, temp2 = first.next, second.next
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        first = head
+        second = prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+
             first.next = second
-            second.next = temp1
-            first = temp1
-            second = temp2
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2
