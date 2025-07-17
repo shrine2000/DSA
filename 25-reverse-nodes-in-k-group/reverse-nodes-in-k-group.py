63778500943994c1243b70c754fb4d8be1de5e61
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-07-17 19:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (86.50%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,42 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        def reverse_segment(start_node, end_node):
-            prev = None
-            curr = start_node
-            while curr != end_node:
-                next_temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_temp
-            return prev
+        if not head:
+            return None
 
-        if not head or k == 1:
-            return head
+        nodes = []
+        curr = head
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
 
-        dummy_head = ListNode(0)
-        dummy_head.next = head
+        n = len(nodes)
 
-        prev_group_end = dummy_head
-
-        while True:
-            k_nodes_start = prev_group_end.next
-            k_nodes_end = k_nodes_start
-            count = 0
-
-            while count < k and k_nodes_end:
-                k_nodes_end = k_nodes_end.next
-                count += 1
-
-            if count < k:
-                break
-
-            next_segment_start = k_nodes_end
-
-            new_group_head = reverse_segment(k_nodes_start, k_nodes_end)
-
-            prev_group_end.next = new_group_head
-
-            k_nodes_start.next = next_segment_start
-            prev_group_end = k_nodes_start
-
-        return dummy_head.next
+        for i in range(0, n, k):
+            if i + k <= n:
+                nodes[i : i + k] = reversed(nodes[i : i + k])
+        dummy = ListNode()
+        curr = dummy
+        for node in nodes:
+            curr.next = node
+            curr = curr.next
+        curr.next = None
+        return dummy.next
