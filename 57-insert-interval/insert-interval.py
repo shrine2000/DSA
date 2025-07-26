ea11066fb2736a0d9234ffa93435ac30a267a5c5
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-07-26 12:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (89.78%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,23 +1,23 @@
+from typing import List
+
 class Solution:
-    def insert(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> List[List[int]]:
-        res = []
-        n, i = len(intervals), 0
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res: List[List[int]] = []
+        idx = 0
+        n = len(intervals)
 
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
+        while idx < n and intervals[idx][1] < newInterval[0]:
+            res.append(intervals[idx])
+            idx += 1
 
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(intervals[i][0], newInterval[0])
-            newInterval[1] = max(intervals[i][1], newInterval[1])
-            i += 1
-
+        while idx < n and intervals[idx][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[idx][0])
+            newInterval[1] = max(newInterval[1], intervals[idx][1])
+            idx += 1
         res.append(newInterval)
 
-        while i < n:
-            res.append(intervals[i])
-            i += 1
+        while idx < n:
+            res.append(intervals[idx])
+            idx += 1
 
         return res
