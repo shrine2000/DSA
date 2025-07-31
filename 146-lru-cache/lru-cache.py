0a9cd86e183b3c1f4e6b7c55609c0efd3eba7aed
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-08-01 00:13:32 +0530

    Time: 152 ms (41.84%) | Memory: 77.8 MB (95.28%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -7,44 +7,45 @@ class Node:
 
 
 class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.cache = {}
+
+    def __init__(self, capacity: int):
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
+        self.capacity = capacity
+        self.cache = {}
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _remove(self, node):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
+    def remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
-    def _add_to_tail(self, node):
+    def add_to_tail(self, node):
         prev = self.tail.prev
         prev.next = node
         node.prev = prev
         node.next = self.tail
         self.tail.prev = node
 
-    def get(self, key):
+    def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
-            self._remove(node)
-            self._add_to_tail(node)
+            self.remove(node)
+            self.add_to_tail(node)
             return node.value
         return -1
 
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._remove(self.cache[key])
+            self.remove(self.cache[key])
         elif len(self.cache) >= self.capacity:
             lru = self.head.next
-            self._remove(lru)
+            self.remove(lru)
             del self.cache[lru.key]
         node = Node(key, value)
-        self._add_to_tail(node)
+        self.add_to_tail(node)
         self.cache[key] = node
 
 
