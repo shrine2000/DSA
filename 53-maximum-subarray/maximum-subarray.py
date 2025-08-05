159adf218b49ac2a93ac58688941d4a2b2c1a238
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-08-05 07:07:26 +0530

    Time: 83 ms (45.68%) | Memory: 31.7 MB (95.97%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,12 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        @lru_cache(maxsize=None)
-        def dp(idx, picked):
-            if idx >= len(nums):
-                return 0 if picked else float("-inf")
-            if picked:
-                return max(0, nums[idx] + dp(idx + 1, True))
-            else:
-                return max(nums[idx] + dp(idx + 1, True), dp(idx + 1, False))
-
-        return dp(0, False)
+        max_sum = curr_sum = nums[0]
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
