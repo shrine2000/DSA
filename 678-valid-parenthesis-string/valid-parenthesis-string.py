9da0a318e2722984fec7a73dc6de3c10f0b406c6
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-08-05 07:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.44%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,21 +1,18 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        min_open = 0
-        max_open = 0
+        low, high = 0, 0
 
         for char in s:
-            if char == "(":
-                min_open += 1
-                max_open += 1
-            elif char == ")":
-                min_open -= 1
-                max_open -= 1
-            else:
-                min_open -= 1
-                max_open += 1
-
-            min_open = max(0, min_open)
-            if max_open < 0:
+            if char == '(':
+                low += 1
+                high += 1
+            elif char == ')':
+                low -= 1
+                high -= 1
+            elif char == '*':
+                low -= 1
+                high += 1
+            low = max(low, 0)
+            if high < 0:
                 return False
-
-        return min_open == 0
+        return low == 0
