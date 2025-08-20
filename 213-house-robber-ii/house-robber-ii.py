8fd607bd59c96dd1551ac00dba73145c14931cb3
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-08-20 19:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.43%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Either rob houses from index 0 to n-2 (exclude last), OR from index 1 to n-1 (exclude first). Then take the max of both cases.
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
         if N == 1:
@@ -9,11 +10,10 @@ class Solution:
             if n == 1:
                 return _nums[0]
             dp = [0] * n
-
             dp[0] = _nums[0]
             dp[1] = max(_nums[0], _nums[1])
             for i in range(2, n):
                 dp[i] = max(dp[i - 1], _nums[i] + dp[i - 2])
             return dp[-1]
 
-        return max(helper(nums[:N - 1]), helper(nums[1:]))
+        return max(helper(nums[: - 1]), helper(nums[1:]))
