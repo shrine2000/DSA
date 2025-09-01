28271609d006b1eb4fba2245e968fd0f808f65a8
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-09-01 23:18:31 +0530

    Time: 50 ms (30.12%) | Memory: 19.3 MB (5.57%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -2,7 +2,7 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        @lru_cache(None)
+        @cache
         def dfs(i, j, k):
             if len(s1) == i and len(s2) == j :
                 return True
