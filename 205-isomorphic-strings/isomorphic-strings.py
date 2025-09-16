87c8a24ecddd83050e8280c5180af0e31370e47e
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-09-16 21:17:12 +0530

    Time: 7 ms (55.80%) | Memory: 17.9 MB (54.49%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        m1 = [0] * 256
+        m2 = [0] * 256
+
+        for idx, (c1, c2) in enumerate(zip(s, t)):
+            if m1[ord(c1)] != m2[ord(c2)]:
+                return False
+
+            m1[ord(c1)] = idx + 1
+            m2[ord(c2)] = idx + 1
+
+        return True
+ 
