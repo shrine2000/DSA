2c587c85eea74e2e24b71514e29e149e43bd53f0
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-09-23 08:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (94.07%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
+
         for num in nums:
-            res ^= num
+            res ^= num 
         return res
