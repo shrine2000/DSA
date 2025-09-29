c34d59b6cde014f7b4f7a6217ec7e50720e98e38
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-09-29 08:56:40 +0530

    Time: 46 ms (37.58%) | Memory: 18.2 MB (39.29%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -15,14 +15,12 @@ class Solution:
         def dfs(graph):
             if not graph:
                 return None
-                
+
             if graph in visited:
                 return visited[graph]
             current = Node(val=graph.val)
             visited[graph] = current
-            for ngbr in graph.neighbors:
-                current.neighbors.append(dfs(ngbr))
+            current.neighbors = [dfs(ngbr) for ngbr in graph.neighbors]
             return current
-        
         return dfs(node)
 
