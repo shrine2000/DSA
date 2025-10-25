7afb8f98fee7a69ab7094b95628af9606440066f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-10-25 19:47:24 +0530

    Time: 267 ms (32.24%) | Memory: 25.4 MB (21.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,34 +1,38 @@
 class Solution:
-    def numIslands(self, grid):
-        if not grid or not grid[0]:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
             return 0
 
+
         R, C = len(grid), len(grid[0])
-        vis = set()
-        count = 0
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
 
-        def bfs(r, c):
-            q = deque([(r, c)])
-            vis.add((r, c))
+        def bfs(i, j):
+            queue = deque([(i, j)])
+            visited.add((i, j))
+            while queue:
+                x, y = queue.popleft()
+                directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+                for dx, dy in directions:
+                    nx, ny = dx + x, dy + y
+                    if (0 <= nx < R and 0 <= ny < C and 
+                        grid[nx][ny] == "1" and (nx, ny) not in visited):
+                        visited.add((nx, ny))
+                        queue.append((nx, ny))
 
-            while q:
-                x, y = q.popleft()
-                for dx, dy in dirs:
-                    nx, ny = x + dx, y + dy
-                    if (
-                        0 <= nx < R
-                        and 0 <= ny < C
-                        and grid[nx][ny] == "1"
-                        and (nx, ny) not in vis
-                    ):
-                        q.append((nx, ny))
-                        vis.add((nx, ny))
+        island = 0
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    island += 1
+                    bfs(i, j)
+        return island
 
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == "1" and (r, c) not in vis:
-                    bfs(r, c)
-                    count += 1
 
-        return count
+
+
+
+
+
+
+            
