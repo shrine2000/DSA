1aa0954af19b87a8139118a105859fa6c970762a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-11-02 21:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (42.43%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        n = len(s)
-        l, r = 0, n - 1
 
-        while l < r and l < n - 1:
-            temp2 =s[l]
-            s[l] = s[r]
-            s[r] = temp2
-            l += 1
-            r -= 1
-        return s
+        n = len(s)
+
+        n = len(s)
+        for i in range(n // 2):
+            s[i], s[n - i - 1] = s[n - i - 1], s[i]
+
+ 
