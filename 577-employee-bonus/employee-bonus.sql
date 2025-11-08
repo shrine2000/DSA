ca41ce32980bb8c1cff37b2cee8250d46330345f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-11-08 12:52:29 +0530

    Time: 290 ms (84.15%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,9 +1,8 @@
-SELECT 
-    e.name, 
-    b.bonus
-FROM 
-    Employee e
-LEFT JOIN 
-    Bonus b ON e.empId = b.empId
-WHERE 
-    COALESCE(b.bonus, 0) < 1000;
+-- Write your PostgreSQL query statement below
+
+
+select e.name, b.bonus
+from Employee as e
+left join Bonus as b
+on e.empId = b.empId
+where b.bonus < 1000 or b.bonus is null
