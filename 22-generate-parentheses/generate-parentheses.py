264faa9100fda1a928c2384c269116706ea9452c
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-11-15 12:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (99.09%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,18 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        stack = [("", 0, 0)]
-        while stack:
-            current, open_count, closed_count = stack.pop()
-            if len(current) == 2 * n:
-                result.append(current)
-                continue
+        res = []
+
+        def dfs(current_str, open_count, closed_count):
+            if len(current_str) == (2 * n):
+                res.append(current_str)
+                return
 
             if open_count < n:
-                stack.append((current + "(", open_count + 1, closed_count))
+                dfs(current_str + "(", open_count + 1, closed_count)
 
             if closed_count < open_count:
-                stack.append((current + ")", open_count, closed_count + 1))
+                dfs(current_str + ")", open_count, closed_count + 1)
 
-        
-        return result
+        dfs("", 0, 0)
+        return res
