010c54c176c38c45cb9e95f327e2c53c19a3242e
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-11-27 09:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.73%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # make dict of parentheses and use stack,
-        #
-        char_map = { ")": "(", "}":"{", "]":"["}
+        char_map = {')': '(', '}': '{', ']':'['}
+
         stack = []
+
         for char in s:
-            if char in char_map.values():
+            if char in ['(', '{', '[']:
                 stack.append(char)
             elif not stack or stack[-1] != char_map[char]:
                 return False
-                stack.append(char)
             else:
                 stack.pop()
         return not stack
-
-
- 
