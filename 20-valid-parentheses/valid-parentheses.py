e0a9f4aea00367a53f0fb01d7e076038148b1b9f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-12-04 23:36:54 +0530

    Time: 2 ms (42.10%) | Memory: 18 MB (15.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,10 +5,10 @@ class Solution:
         stack = []
 
         for char in s:
-            if char in ['(', '{', '[']:
+            if char in char_map.values():
                 stack.append(char)
-            elif not stack or stack[-1] != char_map[char]:
+            elif not stack or  stack[-1] != char_map[char]:
                 return False
-            else:
+            elif stack:
                 stack.pop()
         return not stack
