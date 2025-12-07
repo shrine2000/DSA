9605b43cdbcb8c33ad40ab8e7c9ec4e5d5402d42
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-12-07 12:07:32 +0530

    Time: 257 ms (41.88%) | Memory: 25.3 MB (29.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
+        if not grid or not grid[0]:
             return 0
 
-
         R, C = len(grid), len(grid[0])
-        visited = set()
 
-        def bfs(i, j):
-            queue = deque([(i, j)])
-            visited.add((i, j))
+        dir = [(1, 0), (0, 1), (-1, 0), (0, - 1)]
+        visited =set()
+
+        def bfs(r, c):
+            queue = deque([(r, c)])
+            visited.add((r,c))
             while queue:
                 x, y = queue.popleft()
-                directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-                for dx, dy in directions:
-                    nx, ny = dx + x, dy + y
-                    if (0 <= nx < R and 0 <= ny < C and 
-                        grid[nx][ny] == "1" and (nx, ny) not in visited):
-                        visited.add((nx, ny))
-                        queue.append((nx, ny))
+                for dx, dy in dir:
+                    i = x +dx
+                    j = y + dy
+                    if 0 <= i < R and 0 <= j < C and grid[i][j] == "1" and (i, j) not in visited:
+                        visited.add((i, j))
+                        queue.append((i, j))
 
-        island = 0
+        count = 0
         for i in range(R):
             for j in range(C):
                 if grid[i][j] == "1" and (i, j) not in visited:
-                    island += 1
                     bfs(i, j)
-        return island
+                    count += 1
 
-
-
-
-
-
-
+        return count
 
             
+
+
+
+
