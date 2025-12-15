6538dd572017e3bee996d17267d8332e691cf129
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-12-15 11:55:51 +0530

    Time: 1602 ms (5.02%) | Memory: 595.9 MB (5.00%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,15 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def helper(current, amount):
-            if current >= len(coins) or amount <= 0:
-                return 0 if amount == 0 else float("inf")
-            notTake = 0 + helper(current + 1, amount)
-            take = float("inf")
-            if coins[current] <= amount:
-                take = 1 + helper(current, amount - coins[current])
-            res = min(notTake, take)
-            return res
+        n = len(coins)
+        INF = float("inf")
 
-        result = helper(0, amount)
-        return result if result != float("inf") else -1
+        @lru_cache(None)
+        def dfs(i, rem):
+            if rem == 0:
+                return 0
+            if rem < 0 or i == n:
+                return INF
+            pick = 1 + dfs(i, rem - coins[i])
+            not_pick = dfs(i + 1, rem)
+            return min(pick, not_pick)
+        ans = dfs(0, amount)
+        return -1 if ans == INF else ans
