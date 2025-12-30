3affa6241b54c59eeec7e315ff37294e644db21d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-12-30 08:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (94.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,13 +3,13 @@ class Solution:
         if not intervals:
             return []
 
-        intervals.sort(key=lambda x: x[0])
-        merged = [intervals[0]]
-
-        for current in intervals[1:]:
-            last = merged[-1]
-            if current[0] <= last[1]:
-                last[1] = max(last[1], current[1])
+        intervals.sort(key=lambda x:x[0])
+        # 1, 4 4, 7
+        merged=[]
+        merged.append(intervals[0])
+        for interval in intervals[1:]:
+            if interval[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], interval[1])
             else:
-                merged.append(current)
+                merged.append(interval)
         return merged
