f75455413115069e9d9c3e22b97348b82704fd9b
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-01-24 16:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.99%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,15 +3,14 @@ class Solution:
         res = []
         subset = []
 
-
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset[:])
+        def dfs(idx):
+            if idx == len(nums):
+                res.append(subset.copy())
                 return
-            subset.append(nums[i])
-            dfs(i + 1)
+            subset.append(nums[idx])
+            dfs(idx + 1)
 
             subset.pop()
-            dfs(i + 1)
+            dfs(idx + 1)
         dfs(0)
         return res
