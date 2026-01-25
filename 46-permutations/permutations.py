54961c77703ef82e043d2091dc1525bf4bbfa294
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-01-25 09:18:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (22.17%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,7 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-
+        
         def backtrack(path):
             if len(path) == len(nums):
                 res.append(path.copy())
@@ -10,18 +10,9 @@ class Solution:
             for i in range(len(nums)):
                 if nums[i] in path:
                     continue
-
                 path.append(nums[i])
                 backtrack(path)
                 path.pop()
+            
         backtrack([])
         return res
-
-            
-
-
-
-
-
-
-
