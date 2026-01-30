b5a190a63e1789809acd55287db0774b193d61af
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-01-30 07:38:22 +0530

    Time: 70 ms (48.48%) | Memory: 24.8 MB (23.10%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,10 +3,9 @@ class Solution:
         pq = []
 
         for x, y in points:
-            dist = x *x + y * y
+            dist = x * x + y * y
 
             heapq.heappush(pq, (-dist, [x, y]))
             if len(pq) > k:
                 heapq.heappop(pq)
         return [point for _, point in pq]
-
