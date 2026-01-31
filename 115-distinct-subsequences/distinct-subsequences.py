f86ac6c681eb4bb373875753da070c088affa8f6
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-01-31 19:47:54 +0530

    Time: 415 ms (61.25%) | Memory: 75.7 MB (41.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,14 +1,19 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        @lru_cache(None)
-        def helper(i, j):
-            if len(t) == j:
-                return 1
-            if len(s) == i:
-                return 0
-            if s[i] == t[j]:
-                return helper(i + 1, j + 1) + helper(i + 1, j)
-            else:
-                return helper(i + 1, j)
+        m, n = len(s), len(t)
 
-        return helper(0, 0)
+        if n > m:
+            return 0
+            
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m +1):
+            dp[i][0] = 1
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return dp[m][n]
