eaf10ea189c8722888c7745c43dc8f7899a7fb77
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-04 00:15:36 +0530

    Time: 494 ms (42.55%) | Memory: 52.5 MB (42.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -4,12 +4,12 @@ class Solution:
         n = len(coins)
         dp = [[0] * (amount + 1) for _ in range(n + 1)]
 
-        for i in range(n +1):
+        for i in range(n + 1):
             dp[i][0] = 1
 
         for i in range(n - 1, -1, -1):
             for a in range(1, amount + 1):
                 dp[i][a] = dp[i + 1][a]
-                if a>= coins[i]:
-                    dp[i][a] += dp[i][a -coins[i]]
+                if a >= coins[i]:
+                    dp[i][a] += dp[i][a - coins[i]]
         return dp[0][amount]
