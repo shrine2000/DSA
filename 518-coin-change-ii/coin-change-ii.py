c875781df8123864cd8b1a1e32d26d07d92dca6f
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-04 00:14:59 +0530

    Time: 490 ms (42.86%) | Memory: 52.5 MB (41.76%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        # dp[i][a] = number of ways to make amount a using coins from index i onward
         n = len(coins)
-        @cache
-        def dfs(idx, remaining):
-            if remaining == 0:
-                return 1
-            
-            if remaining < 0 or idx == n:
-                return 0
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
 
-            ways = 0
-            ways += dfs(idx, remaining - coins[idx])
-            ways += dfs(idx + 1, remaining)
-            return ways
-        return dfs(0, amount)
+        for i in range(n +1):
+            dp[i][0] = 1
+
+        for i in range(n - 1, -1, -1):
+            for a in range(1, amount + 1):
+                dp[i][a] = dp[i + 1][a]
+                if a>= coins[i]:
+                    dp[i][a] += dp[i][a -coins[i]]
+        return dp[0][amount]
