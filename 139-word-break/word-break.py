e749686015cc64381f35922131debfb75dcc36f5
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-04 09:09:41 +0530

    Time: 7 ms (22.29%) | Memory: 19.7 MB (8.73%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,13 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         word_set = set(wordDict)
-        n = len(s)
-        dp = [False] * (n + 1)
-        dp[0] = True
+        
+        @cache
+        def dfs(start):
+            if start == len(s):
+                return True
 
-        for i in range(1, n + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in word_set:
-                    dp[i] = True
-                    break
-        return dp[n]
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_set:
+                    if dfs(end):
+                        return True
+            return False
+
+        return dfs(0)
