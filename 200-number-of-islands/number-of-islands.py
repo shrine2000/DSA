dc263b61f27c774d8f4635b636b259b44edc8056
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-09 22:44:13 +0530

    Time: 274 ms (18.38%) | Memory: 31.2 MB (7.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,30 +4,25 @@ class Solution:
             return 0
 
         R, C = len(grid), len(grid[0])
-        dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         visited = set()
 
-        def bfs(r, c):
-            queue = deque([(r, c)])
-            visited.add((r, c))
-            while queue:
-                x, y = queue.popleft()
-                for dx, dy in dir:
-                    i = x + dx
-                    j = y + dy
-                    if (
-                        0 <= i < R
-                        and 0 <= j < C
-                        and grid[i][j] == "1"
-                        and (i, j) not in visited
-                    ):
-                        visited.add((i, j))
-                        queue.append((i, j))
+        def dfs(i, j):
+            if (
+                0 <= i < R
+                and 0 <= j < C
+                and (i, j) not in visited
+                and grid[i][j] == "1"
+            ):
+                visited.add((i, j))
+                dfs(i + 1, j)
+                dfs(i - 1, j)
+                dfs(i, j + 1)
+                dfs(i, j - 1)
 
         count = 0
         for i in range(R):
             for j in range(C):
                 if grid[i][j] == "1" and (i, j) not in visited:
-                    bfs(i, j)
+                    dfs(i, j)
                     count += 1
         return count
