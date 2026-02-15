27a252c3c3c124fcd71d66cf5498ee4667a0b09c
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-15 17:33:00 +0530

    Time: 35 ms (48.99%) | Memory: 19.5 MB (22.64%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,25 +1,26 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        def convert(nums):
-            convert = {
-                "0": 0,
-                "1": 1,
-                "2": 2,
-                "3": 3,
-                "4": 4,
-                "5": 5,
-                "6": 6,
-                "7": 7,
-                "8": 8,
-                "9": 9,
-            }
+    def multiply(self, nums1: str, nums2: str) -> str:
+        if nums1 == "0" or nums2 == "0":
+            return "0"
 
-            i = 1
-            value = 0
-            for num in nums[::-1]:
-                value += convert[num] * i
-                i *= 10
-            return value
+        m = len(nums1)
+        n = len(nums2)
 
-        res = convert(num1) * convert(num2)
-        return str(res)
+        res = [0] * (m + n)
+
+        for i in range(m - 1, -1, - 1):
+            for j in range(n -1, -1, -1):
+                digit1 = ord(nums1[i]) - ord('0')
+                digit2 = ord(nums2[j]) - ord('0')
+
+                product = digit1 * digit2
+
+                pos_low = i + j + 1
+                pos_high = i + j
+
+                total = product + res[pos_low]
+                
+                res[pos_low] = total % 10
+                res[pos_high] += total // 10
+        result_str = ''.join(map(str, res))
+        return result_str.lstrip('0')
