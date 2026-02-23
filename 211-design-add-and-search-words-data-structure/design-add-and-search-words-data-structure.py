5f4778c844806edd0a2913c2ff135e685f629a46
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-23 08:53:33 +0530

    Time: 833 ms (88.05%) | Memory: 68.7 MB (21.87%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,10 +1,11 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.is_end_of_word = False
+        self.is_end = False
 
 
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
 
@@ -14,21 +15,28 @@ class WordDictionary:
             if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-        node.is_end_of_word = True
+        node.is_end = True
 
     def search(self, word: str) -> bool:
-        return self.search_helper(word, self.root)
 
-    def search_helper(self, word, node):
-        if not word:
-            return node.is_end_of_word
-        for i, char in enumerate(word):
+        def dfs(idx, node):
+            if idx == len(word):
+                return node.is_end
+
+            char = word[idx]
             if char == ".":
                 for child in node.children.values():
-                    if self.search_helper(word[i + 1 :], child):
+                    if dfs(idx + 1, child):
                         return True
                 return False
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return node.is_end_of_word
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(idx + 1, node.children[char])
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
