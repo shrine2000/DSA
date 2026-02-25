a20a15a77ca791083dac63b1d28b9b9d844484b6
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-02-25 10:40:26 +0530

    Time: 15 ms (42.83%) | Memory: 19.7 MB (22.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,7 +3,7 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        counter = defaultdict(list)
+        counter = defaultdict(int)
         for char in s:
             counter[char] = counter.get(char, 0) + 1
 
@@ -12,3 +12,8 @@ class Solution:
                 counter[char] = max(0, counter.get(char, 0) - 1)
 
         return bool(all([v == 0 for k, v in counter.items()]))
+
+# check length of both strings,
+# make char frequency map on string s,
+# run for loop on t and reduce count of chars which are in s
+# finally see if count of all chars in intial freq map is zero
