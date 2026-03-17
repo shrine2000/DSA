a989877428255cb3418cf134a80619e470c6f540
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-03-17 23:00:39 +0530

    Time: 60 ms (14.51%) | Memory: 20.5 MB (21.99%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -3,24 +3,16 @@ class Solution:
         m, n = len(s1), len(s2)
         if m + n != len(s3):
             return False
+        @cache
+        def dfs(i, j):
+            k = i + j
+            if k == len(s3):
+                return i == len(s1) and j == len(s2)
 
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
+            if i < len(s1) and s1[i] == s3[k] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[k] and dfs(i, j + 1):
+                return True
+            return False
 
-        for i in range(1, m + 1):
-            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
-
-        for j in range(1, n + 1):
-            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
-
-        print(dp)
-
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                dp[i][j] = (
-                    dp[i - 1][j]
-                    and s1[i - 1] == s3[i + j - 1]
-                    or (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
-                )
-
-        return dp[m][n]
+        return dfs(0, 0)
