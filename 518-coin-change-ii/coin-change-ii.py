17ad62be8af3f65f5dcd7266a69f077c7126712a
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-03-24 07:51:31 +0530

    Time: 1693 ms (5.03%) | Memory: 518.6 MB (5.06%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # dp[i][a] = number of ways to make amount a using coins from index i onward
-        n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n + 1)]
-
-        for i in range(n + 1):
-            dp[i][0] = 1
-
-        for i in range(n - 1, -1, -1):
-            for a in range(1, amount + 1):
-                dp[i][a] = dp[i + 1][a]
-                if a >= coins[i]:
-                    dp[i][a] += dp[i][a - coins[i]]
-        return dp[0][amount]
+        @cache
+        def recursive(idx, remaining):
+            if remaining == 0:
+                return 1
+            if idx >= len(coins) or remaining < 0:
+                return 0
+            pick = recursive(idx, remaining - coins[idx])
+            not_pick = recursive(idx + 1, remaining)
+            return pick + not_pick
+        count = recursive(0, amount)
+        return count
