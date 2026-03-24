17cbdec26a00a7ac51cf779723ef7c1315796186
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-03-24 08:30:33 +0530

    Time: 487 ms (42.57%) | Memory: 52.5 MB (44.34%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,13 +1,27 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def recursive(idx, remaining):
-            if remaining == 0:
-                return 1
-            if idx >= len(coins) or remaining < 0:
-                return 0
-            pick = recursive(idx, remaining - coins[idx])
-            not_pick = recursive(idx + 1, remaining)
-            return pick + not_pick
-        count = recursive(0, amount)
-        return count
+        n = len(coins)
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]  # idx, remaining
+
+        for i in range(n + 1):
+            dp[i][0] = 1
+
+        for idx in range(n - 1, -1, -1):
+            for rem in range(1, amount + 1):
+                dp[idx][rem] = dp[idx + 1][rem]
+
+                if rem >= coins[idx]:
+                    dp[idx][rem] += dp[idx][rem - coins[idx]]
+        return dp[0][amount]
+
+        # @cache
+        # def recursive(idx, remaining):
+        #     if remaining == 0:
+        #         return 1
+        #     if idx >= len(coins) or remaining < 0:
+        #         return 0
+        #     pick = recursive(idx, remaining - coins[idx])
+        #     not_pick = recursive(idx + 1, remaining)
+        #     return pick + not_pick
+        # count = recursive(0, amount)
+        # return count
