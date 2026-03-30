65c9340086094ed3fab159ffeaeca88add4b30ae
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-03-30 09:31:02 +0530

    Time: 260 ms (29.65%) | Memory: 31.1 MB (11.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,26 +5,31 @@ class Solution:
 
         m, n = len(grid), len(grid[0])
 
-        visited=set()
-
+        visited = set()
 
         def dfs(i, j):
-            if (i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != '1' or (i, j) in visited):
+            if (
+                i < 0
+                or j < 0
+                or i >= m
+                or j >= n
+                or grid[i][j] != "1"
+                or (i, j) in visited
+            ):
                 return
 
             visited.add((i, j))
-            
-            dfs(i+1, j)
-            dfs(i, j + 1)
-            dfs(i-1, j)
-            dfs(i, j - 1)
 
+            dfs(i + 1, j)
+            dfs(i, j + 1)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
 
         islands = 0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and (i,j) not in visited:
+                if grid[i][j] == "1" and (i, j) not in visited:
                     dfs(i, j)
                     islands += 1
         return islands
