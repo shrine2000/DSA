78420e0fa85a6c8597d841ca64e12cc3bede8f82
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-04-09 20:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (53.25%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,9 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0 , len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         while l <= r:
-            m = l  + (r - l ) // 2
+            m = (l + r) // 2
+
             if nums[m] == target:
                 return m
 
@@ -12,6 +13,7 @@ class Solution:
                     r = m - 1
                 else:
                     l = m + 1
+
             else:
                 if nums[m] < target <= nums[r]:
                     l = m + 1
