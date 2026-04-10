4762266e2ddf791738a251ff39021f2183ecef3d
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-04-10 09:17:42 +0530

    Time: 639 ms (56.11%) | Memory: 21.9 MB (97.78%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,22 +1,32 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        res=[]
+        res =[]
 
-        for i, a in enumerate(nums):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            l, r = i + 1, len(nums) -1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[left] + nums[right] + nums[i]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
+                    res.append((nums[i], nums[left], nums[right]))
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
         return res
