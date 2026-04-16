7e12318d2235ba07f0e5ef48f2a61d66160ec74e
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-04-16 20:43:24 +0530

    Time: 31 ms (34.85%) | Memory: 27.7 MB (15.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,11 +4,11 @@ class Solution:
         left, right, res = [1] * n, [1] * n, [1] * n
 
         for i in range(1, n):
-            left[i] = left[i - 1] * nums[i - 1]
-        for j in range(n - 2, -1, -1):
-            right[j] = right[j + 1] * nums[j + 1]
-        print(left)
-        print(right)
-        for i in range(n):
-            res[i] = left[i] * right[i]
+            left[i] = left[i- 1] * nums[i - 1]
+
+        for j in range(n - 2, - 1, -1):
+            right[j] = right[j + 1] * nums[j +1]
+
+        for k in range(n):
+            res[k] = left[k] * right[k]
         return res
