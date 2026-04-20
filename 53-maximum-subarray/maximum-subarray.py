1178a4afe9eed0683e764de60ab15adabaee264e
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-04-20 09:02:47 +0530

    Time: 39 ms (48.98%) | Memory: 31.3 MB (77.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -2,7 +2,8 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_profit = float("-inf")
         current_sum = 0
+
         for num in nums:
-            current_sum = max(num + current_sum, num)
+            current_sum = max(num , current_sum + num)
             max_profit = max(max_profit, current_sum)
-        return max_profit
+        return max_profit if max_profit != float('-inf') else 0
