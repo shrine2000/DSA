994fbb09178d7433f2d11d976c832c9e59aaf450
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-05-02 23:47:59 +0530

    Time: 476 ms (92.98%) | Memory: 22.2 MB (81.26%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,31 +4,31 @@ class Solution:
         n = len(nums)
         res = []
 
-        for i in range(0, n - 2):
-            if i > 0 and nums[i - 1] == nums[i]:
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            value = nums[i]
-            left = i + 1
-            right = n - 1
+            if nums[i] > 0:
+                break
 
+            left, right = i + 1, n - 1
             while left < right:
-                val = nums[i] + nums[left] + nums[right]
-                if val == 0:
-                    res.append([nums[i], nums[left], nums[right]])
+                total = nums[i] + nums[left] + nums[right]
 
+                if total < 0:
                     left += 1
+
+                elif total > 0:
                     right -= 1
 
-                    while left < right and nums[left] == nums[left - 1]:
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
 
-                    while left < right and nums[right] == nums[right + 1]:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-                elif val > 0:
-                    right -= 1
-                else:
+
                     left += 1
-
-
+                    right -= 1
         return res
