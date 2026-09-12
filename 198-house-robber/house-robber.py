c7c3ecbeef7583529988deaae24a58b4e29eb610
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-09-12 17:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.90%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,12 +2,11 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         @cache
-        def dfs(i):
-            if i < 0:
-                return  0
-
-            pick  = nums[i] +  dfs(i - 2)
-            not_pick = dfs(i - 1)
-            return max(pick, not_pick)
-
-        return dfs(n - 1)
+        def dfs(idx):
+            if idx >= n:
+                return 0
+            
+            pick = nums[idx] + dfs(idx + 2)
+            skip = dfs(idx + 1)
+            return max(pick, skip)
+        return dfs(0)
