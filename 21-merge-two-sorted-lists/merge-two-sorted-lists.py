e5e253a8d56f5c586ceb821b4abc1d0f68a3b5dc
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-09-13 21:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,6 +9,46 @@ class Solution:
     ) -> Optional[ListNode]:
         dummy = ListNode(0)
         tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next= list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        tail = dummy
         
         while list1 and list2:
             if list1.val <= list2.val:
