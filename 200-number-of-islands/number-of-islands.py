b3d5abb6e7c9e5166cdd5dde8483a8e8ef2f8c36
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-09-13 22:37:09 +0530

    Time: 273 ms (15.11%) | Memory: 26.9 MB (14.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,39 +1,41 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
-            return 0
+            return
 
-        m, n = len(grid), len(grid[0])
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] 
+        self.max_count = 0
         visited = set()
-        max_count = float("-inf")
 
-        def bfs(X, Y):
-            queue = deque([(X, Y)])
-            visited.add((X, Y))
+
+        def is_valid(x, y):
+            return 0 <= x < rows and 0 <= y < cols
+
+
+        def bfs_matrix(i, j):
+            queue = deque([(i, j)])  
+            visited.add((i, j))
             count = 0
 
             while queue:
                 x, y = queue.popleft()
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
-                    if (
-                        0 <= nx < m
-                        and 0 <= ny < n
-                        and (nx, ny) not in visited
-                        and grid[nx][ny] == "1"
-                    ):
-                        count += 1
+                    if is_valid(nx, ny) and (nx, ny) not in visited and grid[nx][ny] == "1":
                         queue.append((nx, ny))
                         visited.add((nx, ny))
-            return count
+    
 
-        islands = 0
-
-        for i in range(m):
-            for j in range(n):
+            
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1" and (i, j) not in visited:
-                    bfs(i, j)
-                    islands += 1
+                    bfs_matrix(i, j)
+                    self.max_count += 1
 
-        return islands
+        return self.max_count
+
+
+
+        
