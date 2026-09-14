434d7c9bce6828b96db12086db9118630f0b2d95
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-09-14 22:07:36 +0530

    Time: 35 ms (24.70%) | Memory: 27.9 MB (7.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,17 +4,19 @@ class Solution:
         # 1 , 1, 2, 3
         # 24, 12, 4, 1
         n = len(nums)
-        left = [1] * n
-        right = [1] * n
+        res = [1] * n
+
+        prefix = [1] * n
+        suffix = [1] * n
         res = [1] * n
 
         for i in range(1, n):
-            left[i] = left[i - 1] * nums[i - 1]
+            prefix[i] = prefix[i - 1] * nums[i - 1]
 
-        for j in range(n - 2, -1, -1):
-            right[j] = right[j + 1] * nums[j + 1]
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
 
-        for k in range(0, n):
-            res[k] = left[k] * right[k]
+        for i in range(n):
+            res[i] = prefix[i] * suffix[i]
 
-        return res
+        return res 
