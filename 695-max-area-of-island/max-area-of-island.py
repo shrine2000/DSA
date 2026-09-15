ad75ce0a1a8f3c2f55962e43f6955a0d2c71c705
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-09-16 00:11:34 +0530

    Time: 12 ms (91.23%) | Memory: 20.6 MB (41.73%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,33 +3,26 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
-        m, n = len(grid), len(grid[0])
-        max_area = 0
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        visited = set()
+        rows, cols = len(grid), len(grid[0])
 
-        def bfs(x, y):
-            queue = deque([(x, y)])
-            visited.add((x, y))
-            area = 1
-            while queue:
-                i, j = queue.popleft()
-                for dx, dy in directions:
-                    nx = dx + i
-                    ny = dy + j
-                    if (
-                        (0 <= nx < m)
-                        and (0 <= ny < n)
-                        and (nx, ny) not in visited
-                        and grid[nx][ny] == 1
-                    ):
-                        queue.append((nx, ny))
-                        visited.add((nx, ny))
-                        area += 1
-            return area
+        max_area = float('-inf')
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1 and (i, j) not in visited:
-                    max_area = max(max_area, bfs(i, j))
-        return max_area
+        def dfs(i, j):
+            if (0 <= i < rows) and ( 0 <= j < cols) and grid[i][j] != 0:
+                grid[i][j] = 0
+                return 1 + (
+                    dfs(i + 1, j) +
+                    dfs(i, j + 1) +
+                    dfs(i - 1, j) +
+                    dfs(i, j - 1) 
+                )
+            return 0
+
+        
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == 1:
+                    max_area = max(max_area, dfs(x, y))
+        return max_area if max_area != float('-inf') else 0
+
+
